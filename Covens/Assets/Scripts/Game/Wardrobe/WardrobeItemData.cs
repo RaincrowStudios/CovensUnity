419ccfe305973ce5ddd58befe7974924ff972ec3
hugo-oktagon <hugo@oktagongames.com>
2018-06-15 17:40:12 -0300

    change: merging master scene again

--- Covens/Assets/Scripts/Game/Wardrobe/WardrobeItemData.cs
+++ Covens/Assets/Scripts/Game/Wardrobe/WardrobeItemData.cs
@@ -4,14 +4,23 @@ using UnityEngine.UI;
 
 public class WardrobeItemData : MonoBehaviour
 {
-	public WardrobeData data; 
 	public Text title;
 	public Image icon;
+
+
+
 	// Use this for initialization
 	void Start ()
 	{
-		title.text = data.type.ToString (); 
+		
 	}
 
+
+
+
+    public void OnClickItem()
+    {
+
+    }
 }
 
