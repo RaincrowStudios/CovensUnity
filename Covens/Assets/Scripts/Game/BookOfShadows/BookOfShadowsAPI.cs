914cee4e6319628e3ab6e4b6293b7aa9fc0473ef
diego-maziero-oktagon <oktagon.engineer4@gmail.com>
2018-07-17 13:39:45 -0300

    Adding Book of Shadows API

---
+++ Covens/Assets/Scripts/Game/BookOfShadows/BookOfShadowsAPI.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class BookOfShadowsAPI
+{
+
+	public static void Display(Action<BookOfShadows_Display> pSuccess, Action<string> pError)
+    {
+        BookOfShadows_Display pDisplayData = new BookOfShadows_Display();
+        CovenManagerAPI.GetCoven<BookOfShadows_Display>("character/display", pDisplayData, pSuccess, pError);
+    }
+}
