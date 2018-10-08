8fd32db55702c8715c74996f675d382bf86a8d99
scarecrow95 <mridul@rainrowgames.com>
2018-10-08 13:34:10 -0400

    New spell casting workflow start

---
+++ Covens/Assets/Scripts/Game/Localize/LocalizationManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic; 
+
+public class LocalizationManager : MonoBehaviour
+{
+	public static Dictionary<string,string> LocalizeDictionary = new Dictionary<string, string> (); 
+	public static event Action OnChangeLanguage;
+
+	class LocalizationData {
+		public string id{ get; set;}
+		public string value{ get; set;	}
+	}
+}
+
+
---
+++ Covens/Assets/Scripts/Game/Localize/LocalizeLookUp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class LocalizeLookUp : MonoBehaviour {
+	Text t;
+	void Awake()
+	{
+		t = GetComponent<Text> ();
+		LocalizationManager.OnChangeLanguage += RefreshText;
+	}
+
+	void OnDestroy(){
+		LocalizationManager.OnChangeLanguage -= RefreshText;
+	}
+
+
+	void RefreshText()
+	{
+		
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
