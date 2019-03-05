cfdb1aebdb5a02bcd27d86189ac7f56395977615
lucas-home <h3nriques@gmail.com>
2019-03-05 11:57:45 -0300

    added player portrait to main UI

--- Covens/Assets/Scripts/Utility/AvatarSpriteUtil.cs
+++ Covens/Assets/Scripts/Utility/AvatarSpriteUtil.cs
@@ -10,9 +10,16 @@ public class AvatarSpriteUtil : MonoBehaviour
         public bool male;
         public List<EquippedApparel> equips;
         public System.Action<Sprite>[] callbacks;
-        public Camera[] cameras;
         public Vector2[] sizes;
         public Vector2[] pivots;
+        public Type[] types;
+    }
+
+    private enum Type
+    {
+        Portrait,
+        Avatar,
+        WardrobePortrait,
     }
 
     public static AvatarSpriteUtil Instance { get; private set; }
@@ -27,7 +34,11 @@ public class AvatarSpriteUtil : MonoBehaviour
     [SerializeField] private Image m_PortraitBackground;
     [SerializeField] private Image m_PortraitFrame;
     [SerializeField] private Camera m_PortraitCamera;
-        
+
+    [Header("Wardrobe Portrait")]
+    [SerializeField] private Image m_PortraitFrame_Wardrobe;
+    [SerializeField] private Camera m_PortraitCamera_Wardrobe;
+
     List<SpriteGenerationSetting> m_Schedule = new List<SpriteGenerationSetting>();
 
     private Coroutine m_Current;
@@ -44,30 +55,15 @@ public class AvatarSpriteUtil : MonoBehaviour
         if (m_PortraitImage == null)
             m_PortraitImage = m_PortraitMask.GetComponent<Image>();
 
-        m_PortraitCamera.gameObject.SetActive(false);
-        m_FullbodyCamera.gameObject.SetActive(false);
         m_PortraitMask.enabled = false;
         m_PortraitImage.enabled = false;
         m_PortraitFrame.enabled = false;
         m_PortraitBackground.enabled = false;
-    }
+        m_PortraitFrame_Wardrobe.enabled = false;
 
-    public void GeneratePortraitSprite(bool male, List<EquippedApparel> equips, System.Action<Sprite> callback)
-    {
-        SpriteGenerationSetting prop = new SpriteGenerationSetting
-        {
-            male = male,
-            equips = equips,
-            callbacks = new System.Action<Sprite>[] { callback },
-            cameras = new Camera[] { m_PortraitCamera },
-            sizes = new Vector2[] { new Vector2(256, 256) },
-            pivots = new Vector2[] { new Vector2(0.5f, 0.5f) }
-        };
-
-        if (m_Current == null)
-            m_Current = StartCoroutine(GenerateSpriteCoroutine(prop));
-        else
-            m_Schedule.Insert(0, prop); //portraits are priority
+        m_PortraitCamera.enabled = false;
+        m_FullbodyCamera.enabled = false;
+        m_PortraitCamera_Wardrobe.enabled = false;
     }
 
     public void GenerateFullbodySprite(bool male, List<EquippedApparel> equips, System.Action<Sprite> callback)
@@ -77,9 +73,9 @@ public class AvatarSpriteUtil : MonoBehaviour
             male = male,
             equips = equips,
             callbacks = new System.Action<Sprite>[] { callback },
-            cameras = new Camera[] { m_FullbodyCamera },
             sizes = new Vector2[] { new Vector2(256, 500) },
-            pivots = new Vector2[] { new Vector2(0.5f, 0.0f) }
+            pivots = new Vector2[] { new Vector2(0.5f, 0.0f) },
+            types = new Type[] { Type.Avatar }
         };
 
         if (m_Current == null)
@@ -95,15 +91,33 @@ public class AvatarSpriteUtil : MonoBehaviour
             male = male,
             equips = equips,
             callbacks = new System.Action<Sprite>[] { portraitCallback, avatarCallback },
-            cameras = new Camera[] { m_PortraitCamera, m_FullbodyCamera },
             sizes = new Vector2[] { new Vector2(256, 256), new Vector2(256, 500) },
-            pivots = new Vector2[] { new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.0f) }
+            pivots = new Vector2[] { new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.0f) },
+            types = new Type[] { Type.Portrait, Type.Avatar }
         };
 
         if (m_Current == null)
             m_Current = StartCoroutine(GenerateSpriteCoroutine(prop));
         else
             m_Schedule.Add(prop);
+    }
+
+    public void GenerateWardrobePortrait(System.Action<Sprite> callback)
+    {
+        SpriteGenerationSetting sett = new SpriteGenerationSetting
+        {
+            male = PlayerDataManager.playerData.male,
+            equips = PlayerDataManager.playerData.equipped,
+            callbacks = new System.Action<Sprite>[] { callback },
+            sizes = new Vector2[] { new Vector2(256, 256) },
+            pivots = new Vector2[] { new Vector2(0.5f, 0.5f) },
+            types = new Type[] { Type.WardrobePortrait }
+        };
+
+        if (m_Current == null)
+            m_Current = StartCoroutine(GenerateSpriteCoroutine(sett));
+        else
+            m_Schedule.Add(sett);
     }
 
     private IEnumerator GenerateSpriteCoroutine(SpriteGenerationSetting properties)
@@ -118,6 +132,7 @@ public class AvatarSpriteUtil : MonoBehaviour
         bool prevRootState = root.gameObject.activeSelf;
         Vector3 prevRootPos = root.transform.position;
         bool prevState = characterView.gameObject.activeSelf;
+        Camera cam;
 
         //move the character out of screen, and set it up
         root.transform.position = new Vector3(-1000, 0, 0);
@@ -126,39 +141,69 @@ public class AvatarSpriteUtil : MonoBehaviour
         characterView.InitializeChar(properties.equips);
 
         //generate the sprites for each camera passed
-        for (int i = 0; i < properties.cameras.Length; i++)
+        for (int i = 0; i < properties.callbacks.Length; i++)
         {
             yield return new WaitForEndOfFrame();
+                                   
+            if (properties.types[i] == Type.Portrait)
+            {
+                m_PortraitImage.enabled = true;
+                m_PortraitMask.enabled = true;
+                m_PortraitFrame.enabled = true;
+                m_PortraitBackground.enabled = true;
 
-            bool isPortrait = properties.cameras[i] == m_PortraitCamera;
-            m_PortraitImage.enabled = isPortrait;
-            m_PortraitMask.enabled = isPortrait;
-            m_PortraitFrame.enabled = isPortrait;
-            m_PortraitBackground.enabled = isPortrait;
+                cam = m_PortraitCamera;
+            }
+            else if (properties.types[i] == Type.WardrobePortrait)
+            {
+                m_PortraitImage.enabled = true;
+                m_PortraitMask.enabled = true;
+                m_PortraitFrame_Wardrobe.enabled = true;
 
-            properties.cameras[i].gameObject.SetActive(true);
+                cam = m_PortraitCamera_Wardrobe;
+            }
+            else
+            {
+                cam = m_FullbodyCamera;
+            }
+
+            cam.enabled = true;
 
             //generate the texture and sprite
             int width = (int)properties.sizes[i].x;
             int height = (int)properties.sizes[i].y;
             RenderTexture rt = new RenderTexture(width, height, 32);
-            properties.cameras[i].targetTexture = rt;
+            cam.targetTexture = rt;
             Texture2D texture = new Texture2D(width, height, TextureFormat.RGBA32, false);
-            properties.cameras[i].Render();
+            cam.Render();
             RenderTexture prev = RenderTexture.active;
             RenderTexture.active = rt;
             texture.ReadPixels(new Rect(0, 0, width, height), 0, 0);
             texture.Apply();
             RenderTexture.active = prev;
-            properties.cameras[i].targetTexture = null;
+            cam.targetTexture = null;
             Sprite sprite = Sprite.Create(texture, new Rect(0, 0, width, height), properties.pivots[i]);
+            cam.enabled = false;
 
-            properties.cameras[i].gameObject.SetActive(false);
             properties.callbacks[i]?.Invoke(sprite);
 
 #if UNITY_EDITOR
             SaveTexture(texture);
 #endif
+
+            if (properties.types[i] == Type.Portrait)
+            {
+                m_PortraitImage.enabled = false;
+                m_PortraitMask.enabled = false;
+                m_PortraitFrame.enabled = false;
+                m_PortraitBackground.enabled = false;
+            }
+            else if (properties.types[i] == Type.WardrobePortrait)
+            {
+                m_PortraitImage.enabled = false;
+                m_PortraitMask.enabled = false;
+                m_PortraitFrame_Wardrobe.enabled = false;
+            }
         }
         
         //reset character to initial state
@@ -189,7 +234,7 @@ public class AvatarSpriteUtil : MonoBehaviour
     private void GenerateDebugPortrait()
     {
         MarkerDataDetail markerdata = Newtonsoft.Json.JsonConvert.DeserializeObject<MarkerDataDetail>(m_EquipList);
-        GeneratePortraitSprite(markerdata.male, markerdata.equipped, (spr) => m_Renderer.sprite = spr);
+        //GeneratePortraitSprite(markerdata.male, markerdata.equipped, (spr) => m_Renderer.sprite = spr);
     }
 
     [ContextMenu("Debug avatar")]
---
+++ Covens/Assets/Scripts/Utility/UI/LoadPlayerPortrait.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadPlayerPortrait : MonoBehaviour {
+
+    [SerializeField] private UnityEngine.UI.Image m_Image;
+
+    private void Awake()
+    {
+        if (m_Image == null)
+            m_Image = GetComponent<UnityEngine.UI.Image>();
+
+        m_Image.color = new Color(0, 0, 0, 0);
+
+        StartCoroutine(WaitForPlayerdata());
+    }
+
+    private IEnumerator WaitForPlayerdata()
+    {
+        while (PlayerDataManager.playerData == null)
+        {
+            yield return 60;
+        }
+
+        AvatarSpriteUtil.Instance.GenerateWardrobePortrait(spr =>
+        {
+            m_Image.sprite = spr;
+            m_Image.color = new Color(1, 1, 1, 1);
+        });
+    }
+}
