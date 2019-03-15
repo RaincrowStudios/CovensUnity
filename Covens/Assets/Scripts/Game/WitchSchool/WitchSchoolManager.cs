7c02f18572e23adf6d008e9672dda987e988ec70
scarecrow95 <mridul@rainrowgames.com>
2019-03-15 11:33:16 -0400

    fixed witchschool closing,added a check for lucas's hot witch appearing while playing ftf

--- Covens/Assets/Scripts/Game/WitchSchool/WitchSchoolManager.cs
+++ Covens/Assets/Scripts/Game/WitchSchool/WitchSchoolManager.cs
@@ -3,89 +3,97 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class WitchSchoolManager : MonoBehaviour {
-	public static WitchSchoolManager Instance { get; set;}
-	public static List<LocalizeData> witchVideos = new List< LocalizeData> ();
+public class WitchSchoolManager : MonoBehaviour
+{
+    public static WitchSchoolManager Instance { get; set; }
+    public static List<LocalizeData> witchVideos = new List<LocalizeData>();
 
-	public GameObject videoItem;
-	public Transform container;
-	public CanvasGroup CG;
-	public Animator anim;
-	public MediaPlayerCtrl player;
-	public Text videoTitle;
-	void Awake()
-	{
-		Instance = this;
-	}
+    public GameObject videoItem;
+    public Transform container;
+    public CanvasGroup CG;
+    public Animator anim;
+    public MediaPlayerCtrl player;
+    public Text videoTitle;
+    void Awake()
+    {
+        Instance = this;
+    }
 
-	public void Open()
-	{
-		anim.gameObject.SetActive (true);
-		anim.SetBool ("open", true);
+    public void Open()
+    {
+        anim.gameObject.SetActive(true);
+        container.localScale = Vector3.zero;
+        LeanTween.scale(container.gameObject, Vector3.one, .6f).setEase(LeanTweenType.easeOutQuad);
+        //anim.SetBool ("open", true);
 
-	}
+    }
 
-	void Start()
-	{
-		foreach (var item in witchVideos) {
-			var g = Utilities.Instantiate (videoItem, container);
-			g.GetComponent<witchSchoolData> ().Setup (item);
-		}
-	}
+    void Start()
+    {
+        foreach (var item in witchVideos)
+        {
+            var g = Utilities.Instantiate(videoItem, container);
+            g.GetComponent<witchSchoolData>().Setup(item);
+        }
+    }
 
-	public void Close()
-	{
-		anim.SetBool ("open", false);
-		Disable (anim.gameObject, 1.5f);
-	}
+    public void Close()
+    {
+        //	anim.SetBool ("open", false);
+        //Disable (anim.gameObject, 1.5f);
+        LeanTween.scale(container.gameObject, Vector3.zero, .4f).setEase(LeanTweenType.easeOutQuad).setOnComplete(() => transform.gameObject.SetActive(false));
+    }
 
-	public void playVideo(string URL,string title){
-		videoTitle.text = title;
-		StartCoroutine (FadeInFocus (CG));
-		player.Load (URL);
-	}
+    public void playVideo(string URL, string title)
+    {
+        videoTitle.text = title;
+        StartCoroutine(FadeInFocus(CG));
+        player.Load(URL);
+    }
 
-	public void CloseVideo()
-	{
-		player.UnLoad ();
-		StartCoroutine (FadeOutFocus (CG));
+    public void CloseVideo()
+    {
+        player.UnLoad();
+        StartCoroutine(FadeOutFocus(CG));
 
-	}
+    }
 
-	IEnumerator FadeOutFocus (CanvasGroup cg)
-	{
+    IEnumerator FadeOutFocus(CanvasGroup cg)
+    {
 
-		float t = 0;
-		while (t <= 1) {
-			t += Time.deltaTime * 2.8f;
-			cg.alpha = Mathf.SmoothStep (1, 0, t);
-			yield return 0;
-		}
-		cg.gameObject.SetActive (false);
+        float t = 0;
+        while (t <= 1)
+        {
+            t += Time.deltaTime * 2.8f;
+            cg.alpha = Mathf.SmoothStep(1, 0, t);
+            yield return 0;
+        }
+        cg.gameObject.SetActive(false);
 
-	}
+    }
 
-	IEnumerator FadeInFocus (CanvasGroup cg, float delay = 0)
-	{
+    IEnumerator FadeInFocus(CanvasGroup cg, float delay = 0)
+    {
 
-		cg.gameObject.SetActive (true);
-		float t = 0;
-		while (t <= 1) {
-			t += Time.deltaTime * 2;
-			cg.alpha = Mathf.SmoothStep (0, 1, t);
-			yield return 0;
-		}
+        cg.gameObject.SetActive(true);
+        float t = 0;
+        while (t <= 1)
+        {
+            t += Time.deltaTime * 2;
+            cg.alpha = Mathf.SmoothStep(0, 1, t);
+            yield return 0;
+        }
 
-	}
+    }
 
-	public void Disable (GameObject g, float delay = 1.5f)
-	{
-		StartCoroutine (disableObject (g, delay));
-	}
+    public void Disable(GameObject g, float delay = 1.5f)
+    {
+        StartCoroutine(disableObject(g, delay));
+    }
 
-	IEnumerator disableObject (GameObject g, float delay)
-	{
-		yield return new WaitForSeconds (delay);
-		g.SetActive (false);
-	}
+    IEnumerator disableObject(GameObject g, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        g.SetActive(false);
+    }
 }
