8cfcd045cd1f9d7fbb9dec98adcd002a97d61e04
lucas henriques <oktagon.engineer1@gmail.com>
2019-03-18 17:04:14 -0300

    [change] remove mask from PlayerFeed, replaced default UI material, replace Text for TMP Text.

--- Covens/Assets/Scripts/Game/PlayerFeed/LogScroller.cs
+++ Covens/Assets/Scripts/Game/PlayerFeed/LogScroller.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using EnhancedUI;
 using EnhancedUI.EnhancedScroller;
 using UnityEngine.UI;
+using TMPro;
 
 public class LogScroller : MonoBehaviour, IEnhancedScrollerDelegate
 {
@@ -40,19 +41,19 @@ public class LogScroller : MonoBehaviour, IEnhancedScrollerDelegate
 	}
 
 	void SetupText (EnhancedScrollerCellView text, EventLogData data){
-		var t = text.GetComponent<Text> ();
+		var t = text.GetComponent<TextMeshProUGUI> ();
 		if (data.type == "dailyBlessing") {
 			t.text = "Savannah Grey granted you her <b>daily blessing</b>. Long live the Deal! <color=#FF9900FF>+" + data.energyChange.ToString () + " energy </color><size=35> [" + GetTimeStamp (data.timestamp) + "]</size>";
-			t.transform.GetChild (0).GetComponent<Text> ().text = GetDayStamp (data.timestamp);
+			t.transform.GetChild (0).GetComponent<TextMeshProUGUI> ().text = GetDayStamp (data.timestamp);
 		} else if (data.type == "lunarBlessing") {
 			t.text = "The <b>Moon</b> is in your <b>favor</b>.<color=#FF9900FF>+" + data.energyChange.ToString () + " energy </color><size=35> [" + GetTimeStamp (data.timestamp) + "]</size>";
-			t.transform.GetChild (0).GetComponent<Text> ().text = GetDayStamp (data.timestamp);
+			t.transform.GetChild (0).GetComponent<TextMeshProUGUI> ().text = GetDayStamp (data.timestamp);
 		} else if (data.type == "uponSpiritBorn") {
 			t.text = "Your <b>" + DownloadedAssets.spiritDictData [data.spirit].spiritName + "</b> has entered the world. <size=35> [" + GetTimeStamp (data.timestamp) + "]</size>";
-			t.transform.GetChild (0).GetComponent<Text> ().text = GetDayStamp (data.timestamp);
+			t.transform.GetChild (0).GetComponent<TextMeshProUGUI> ().text = GetDayStamp (data.timestamp);
 		} else if (data.type == "ifSpiritFlips") {
 			t.text = "Your <b>" + DownloadedAssets.spiritDictData [data.spirit].spiritName + "</b> has turned against you!.<color=red>-" + data.energyChange.ToString () + " energy </color><size=35> [" + GetTimeStamp (data.timestamp) + "]</size>";
-			t.transform.GetChild (0).GetComponent<Text> ().text = GetDayStamp (data.timestamp);
+			t.transform.GetChild (0).GetComponent<TextMeshProUGUI> ().text = GetDayStamp (data.timestamp);
 		} else if (data.type == "spellCast") {
 			print ("SPELLCAST!");
 			string school = "";
@@ -71,10 +72,10 @@ public class LogScroller : MonoBehaviour, IEnhancedScrollerDelegate
 				t.text = "The " + school + " <b>" + data.casterName + "</b> cast <b>" + DownloadedAssets.spellDictData [data.spellId].spellName + " </b>on you. [" + GetTimeStamp (data.timestamp) + "]</size>";
 			}
 
-			t.transform.GetChild (0).GetComponent<Text> ().text = GetDayStamp (data.timestamp);
+			t.transform.GetChild (0).GetComponent<TextMeshProUGUI> ().text = GetDayStamp (data.timestamp);
 		} else if (data.type == "sentinel") {
 			t.text = "Your <b> Sentinel Owl </b> has spotted <color=red>" + DownloadedAssets.spiritDictData [data.spiritId].spiritName + "</color>.<size=35> [" + GetTimeStamp (data.timestamp) + "]</size>";
-			t.transform.GetChild (0).GetComponent<Text> ().text = GetDayStamp (data.timestamp);
+			t.transform.GetChild (0).GetComponent<TextMeshProUGUI> ().text = GetDayStamp (data.timestamp);
 		}
 	}
 
