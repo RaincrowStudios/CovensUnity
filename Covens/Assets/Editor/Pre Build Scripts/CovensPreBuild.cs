9755a069d5a5ec3b0e89d2ab1ce103a27aaa318a
Emanuel Tavares <emanuel.oktagon@gmail.com>
2019-03-21 19:14:17 -0300

    [Fix] Improved Covens Pre-Build

--- Covens/Assets/Editor/Pre Build Scripts/CovensPreBuild.cs
+++ Covens/Assets/Editor/Pre Build Scripts/CovensPreBuild.cs
@@ -9,6 +9,50 @@ public class CovensPreBuild
     [MenuItem("Raincrow/Pre Build/Run")]
     public static void Start()
     {
+        CleanupWorktree();
+        UpdateVersionAndBuildNumber();
+        GitCommitChanges();
+    }    
+
+    private static void CleanupWorktree()
+    {
+        ProcessStartInfo startInfo = new ProcessStartInfo
+        {
+            FileName = "git.exe",
+            Arguments = "reset --hard",
+            UseShellExecute = false,
+            RedirectStandardOutput = true
+        };
+
+        using (Process gitProcess = Process.Start(startInfo))
+        {
+            using (System.IO.StreamReader reader = gitProcess.StandardOutput)
+            {
+                gitProcess.WaitForExit();
+                Debug.LogFormat("{0} {1}", gitProcess.StartInfo.FileName, gitProcess.StartInfo.Arguments);
+            }
+        }
+
+        startInfo = new ProcessStartInfo
+        {
+            FileName = "git.exe",
+            Arguments = "clean -df",
+            UseShellExecute = false,
+            RedirectStandardOutput = true
+        };
+
+        using (Process gitProcess = Process.Start(startInfo))
+        {
+            using (System.IO.StreamReader reader = gitProcess.StandardOutput)
+            {
+                gitProcess.WaitForExit();
+                Debug.LogFormat("{0} {1}", gitProcess.StartInfo.FileName, gitProcess.StartInfo.Arguments);
+            }
+        }
+    }
+
+    private static void UpdateVersionAndBuildNumber()
+    {
         try
         {
             // Get branch name
@@ -60,23 +104,62 @@ public class CovensPreBuild
             }
 
             PlayerSettings.bundleVersion = branchNumberString;
-            Debug.LogFormat("Bundle Version: {0}", PlayerSettings.bundleVersion);
+            Debug.LogFormat("App Version: {0}", PlayerSettings.bundleVersion);
 
             PlayerSettings.Android.bundleVersionCode = commitCount;
-            Debug.LogFormat("Bundle Version: {0}", PlayerSettings.Android.bundleVersionCode);
+            Debug.LogFormat("Android Bundle Version: {0}", PlayerSettings.Android.bundleVersionCode);
 
             PlayerSettings.iOS.buildNumber = commitCount.ToString();
-            Debug.LogFormat("Bundle Version: {0}", PlayerSettings.iOS.buildNumber);
+            Debug.LogFormat("iOS Bundle Version: {0}", PlayerSettings.iOS.buildNumber);
 
             // This will never be a problem anymore
             PlayerSettings.SplashScreen.show = false;
             Debug.LogFormat("Show Splash Screen: {0}", PlayerSettings.SplashScreen.show ? "Yes" : "No");
 
-
-        }        
+            AssetDatabase.SaveAssets();
+        }
         catch (System.Exception e)
         {
             Debug.LogException(e);
+        }
+    }
+
+    private static void GitCommitChanges()
+    {
+        string projectAssetPath = Application.dataPath.Replace("/Assets", "/ProjectSettings/ProjectSettings.asset");
+
+        ProcessStartInfo startInfo = new ProcessStartInfo
+        {
+            FileName = "git.exe",
+            Arguments = string.Concat("add ", projectAssetPath),
+            UseShellExecute = false,
+            RedirectStandardOutput = true
+        };
+
+        using (Process gitProcess = Process.Start(startInfo))
+        {
+            using (System.IO.StreamReader reader = gitProcess.StandardOutput)
+            {
+                gitProcess.WaitForExit();
+            }
+        }
+
+        string commitMessage = string.Format("[New] Update Version {0} - Bundle Version {1} - Build Number {2}", PlayerSettings.bundleVersion, PlayerSettings.Android.bundleVersionCode, PlayerSettings.iOS.buildNumber);
+
+        startInfo = new ProcessStartInfo
+        {
+            FileName = "git.exe",
+            Arguments = string.Format("commit -m \"{0}\"", commitMessage),
+            UseShellExecute = false,
+            RedirectStandardOutput = true
+        };
+
+        using (Process gitProcess = Process.Start(startInfo))
+        {
+            using (System.IO.StreamReader reader = gitProcess.StandardOutput)
+            {
+                gitProcess.WaitForExit();
+            }
         }
     }
 }
