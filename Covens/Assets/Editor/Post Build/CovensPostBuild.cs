bd6f2b188221d1bfbb119a13935c96d881963fb7
Emanuel Tavares <emanuel.oktagon@gmail.com>
2019-03-28 15:07:10 -0300

    [Fix] Added NSLocationAlwaysUsageDescription to plist

---
+++ Covens/Assets/Editor/Post Build/CovensPostBuild.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEditor.iOS.Xcode;
+using UnityEngine;
+
+public class CovensPostBuild : MonoBehaviour
+{
+    static void OnPostprocessBuild(BuildTarget buildTarget, string path)
+    {
+        
+        var plistPath = System.IO.Path.Combine(path, "Info.plist");
+        var plist = new PlistDocument();
+        plist.ReadFromFile(plistPath);
+
+        // Update value
+        PlistElementDict rootDict = plist.root;
+        rootDict.SetString("NSLocationAlwaysUsageDescription", "Covens uses your real location data in order to play the game.");
+
+        // Write plist
+        System.IO.File.WriteAllText(plistPath, plist.WriteToString());
+    }
+}
