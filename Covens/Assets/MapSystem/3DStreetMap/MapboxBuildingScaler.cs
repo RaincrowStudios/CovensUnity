8889e6653ae87c5b695042176984dbb54b7027ef
lucas henriques <oktagon.engineer1@gmail.com>
2019-04-03 11:02:08 -0300

    [adjust] changed mapbox building scaling ease type.

--- Covens/Assets/MapSystem/3DStreetMap/MapboxBuildingScaler.cs
+++ Covens/Assets/MapSystem/3DStreetMap/MapboxBuildingScaler.cs
@@ -16,7 +16,7 @@ public class MapboxBuildingScaler : MonoBehaviour
             m_Zoom = m_MapController.normalizedZoom;
             m_BuildingsParent.localScale = new Vector3(
                 m_BuildingsParent.localScale.x,
-                m_BuildingsParent.localScale.x * (1 - m_Zoom) + 0.001f,
+                m_BuildingsParent.localScale.x * LeanTween.easeOutQuint(0, 1, 1 - m_Zoom) + 0.001f,
                 m_BuildingsParent.localScale.z
             );
         }
