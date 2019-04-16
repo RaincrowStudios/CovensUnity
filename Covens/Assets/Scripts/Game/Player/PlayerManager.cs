9bc6cd78a04df9c4e80cbedd2a4bbba04b3745ed
ocpaynter4 <47328047+ocpaynter4@users.noreply.github.com>
2019-04-16 11:55:12 -0400

    Refocus camera on witch selection to center the witch, add dynamic ring to player marker that changes color based on degree (particle system)

--- Covens/Assets/Scripts/Game/Player/PlayerManager.cs
+++ Covens/Assets/Scripts/Game/Player/PlayerManager.cs
@@ -34,6 +34,8 @@ public class PlayerManager : MonoBehaviour
     public static IMarker physicalMarker { get; set; }       // gyro marker
     public static WitchMarker witchMarker { get; private set; }
 
+	[SerializeField] private GameObject selectionRing;
+
     public static bool inSpiritForm
     {
         get
@@ -190,8 +192,28 @@ public class PlayerManager : MonoBehaviour
         OnUpdateEquips();
 
         marker.gameObject.transform.SetParent(this.transform);
+		selectionRing =  marker.gameObject.transform.GetChild (0).GetChild (1).gameObject;
 
 
+
+		if (PlayerDataManager.playerData.degree > 0) {
+			selectionRing.transform.GetChild (0).gameObject.SetActive (true);
+			selectionRing.transform.GetChild (1).gameObject.SetActive (false);
+			selectionRing.transform.GetChild (2).gameObject.SetActive (false);
+		}
+		else if (PlayerDataManager.playerData.degree < 0) {
+			selectionRing.transform.GetChild (0).gameObject.SetActive (false);
+			selectionRing.transform.GetChild (1).gameObject.SetActive (false);
+			selectionRing.transform.GetChild (2).gameObject.SetActive (true);
+		}
+		else {
+			selectionRing.transform.GetChild (0).gameObject.SetActive (false);
+			selectionRing.transform.GetChild (1).gameObject.SetActive (true);
+			selectionRing.transform.GetChild (2).gameObject.SetActive (false);
+		}
+
+		// update ring 
+		//PDM.playerData.degree
         //		StartCoroutine()
         AddAttackRing();
     }
