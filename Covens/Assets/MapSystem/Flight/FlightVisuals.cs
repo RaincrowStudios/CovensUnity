36d8cc5dc942c8ee10dd1294a895925b6e87ea28
ocpaynter4 <47328047+ocpaynter4@users.noreply.github.com>
2019-04-25 12:06:43 -0400

    Added animation to Flight Icon, and fixed Flight Icon not appearing on first flight

--- Covens/Assets/MapSystem/Flight/FlightVisuals.cs
+++ Covens/Assets/MapSystem/Flight/FlightVisuals.cs
@@ -14,6 +14,7 @@ public class FlightVisuals : MonoBehaviour
     [SerializeField] private TextMeshProUGUI m_LabelTitle;
     [SerializeField] private TextMeshProUGUI m_LabelSubtitle;
 
+	public GameObject FlyFX;
 	public SpriteRenderer fx;
 	public SpriteRenderer fx1;
 	//public GameObject UIFlyGlow;
@@ -32,32 +33,44 @@ public class FlightVisuals : MonoBehaviour
     private Vector3 m_LastMapPosition;
     private Vector3 m_TargetPosition;
 
+	public void Start()
+	{
+		
+		//LeanTween.scale (FlyFX, Vector3.zero, 0.1f);
+	}
     private void Awake()
     {
 		
-        gameObject.SetActive(false);
+        //gameObject.SetActive(false);
 		IconFXColor ();
+		Debug.Log ("Awake");
     }
 
     private void OnEnable()
     {
         MapsAPI.Instance.OnChangePosition = OnMapPan;
         m_LastMapPosition = MapsAPI.Instance.GetWorldPosition();
+		Debug.Log ("OnEnable");
+
     }
 
     private void OnDisable()
     {
+		Debug.Log ("OnDisable");
         MapsAPI.Instance.OnChangePosition -= OnMapPan;
     }
 
     private void OnMapPan()
     {
+		Debug.Log ("OnMapPan");
         Vector3 newPos = MapsAPI.Instance.GetWorldPosition();
         Vector3 delta = (newPos - m_LastMapPosition);
         m_LastMapPosition = newPos;
 
     }
 	public void IconFXColor() {
+		Debug.Log ("IconFXColor");
+
 		if (PlayerDataManager.playerData.degree > 0) {
 			fx.color = new Color (1f, 1f, 1f);
 			fx1.color = new Color (1f, 0.59f, 0f);
@@ -73,20 +86,28 @@ public class FlightVisuals : MonoBehaviour
 
     public void StartFlight()
     {
+		FlyFX = transform.GetChild (0).GetChild (0).gameObject;
+		Debug.Log ("StartFlight");
+
         m_LastMapPosition = MapsAPI.Instance.GetWorldPosition();
         gameObject.SetActive(true);
+		LeanTween.scale (FlyFX, Vector3.one, 0.3f);
         (PlayerManager.marker as WitchMarker).GetPortrait(spr => m_PlayerPortrait.sprite = spr);
 		DeathState.Instance.FlightGlowOn ();
     }
 
     public void EndFlight()
     {
+		Debug.Log ("EndFlight");
 		DeathState.Instance.FlightGlowOff ();
-        gameObject.SetActive(false);
+		LeanTween.scale (FlyFX, Vector3.zero, 0.6f).setOnComplete(() => {
+			gameObject.SetActive(false);
+		});
     }
 
     public void UpdateUI(string title, string subtitle)
     {
+		Debug.Log ("UpdateUI");
         m_LabelTitle.text = title;
         m_LabelSubtitle.text = subtitle;
     }
