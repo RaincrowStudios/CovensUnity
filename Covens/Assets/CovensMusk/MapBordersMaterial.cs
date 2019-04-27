8cc9a2ac7d3120c70a74b5506cf7e2e0417408f0
lucas-home <h3nriques@gmail.com>
2019-04-26 23:37:51 -0300

    added dark borders to the worldmap.

---
+++ Covens/Assets/CovensMusk/MapBordersMaterial.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapBordersMaterial : MonoBehaviour
+{
+    [SerializeField] private Material m_Material;
+    [SerializeField] private CovensMuskMap m_Maps;
+    [SerializeField] private MapCameraController m_Controller;
+
+    [Header("Debug - do not change")]
+    public Material m_MaterialInstance;
+    
+    private void Awake()
+    {
+        m_Controller.onUpdate += OnMapUpdate;
+    }
+
+    private void OnEnable()
+    {
+        if (Application.isPlaying == false)
+            return;
+
+        if (m_MaterialInstance == null)
+            m_MaterialInstance = new Material(m_Material);
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, m_MaterialInstance);
+    }
+
+    private void OnMapUpdate(bool pos, bool zoom, bool rot)
+    {
+        double lng, lat;
+        m_Maps.GetCoordinates(out lng, out lat);
+
+        Vector2 topLeft = m_Controller.camera.WorldToViewportPoint(m_Maps.topLeftBorder);
+        Vector2 botRight = m_Controller.camera.WorldToViewportPoint(m_Maps.botRightBorder);
+        Rect bounds = new Rect(0, 0, 1, 1);
+
+        if(lng < -100)
+            bounds.x = topLeft.x;
+        else if (lng > 100)
+            bounds.width = botRight.x;
+
+        if (lat < -30)
+            bounds.y = botRight.y;
+        else if (lat > 30)
+            bounds.height = topLeft.y;
+
+        m_MaterialInstance.SetVector("_Screen", new Vector4(bounds.x, bounds.y, bounds.width, bounds.height));
+    }
+}
---
+++ Covens/Assets/CovensMusk/Shaders/MapBorders.shader
@@ -0,0 +1,63 @@
+﻿
+Shader "Covens/MapBorders"
+{
+	Properties {
+		_MainTex ("Base (RGB)", 2D) = "white" {}
+		_Color ("Color", Color) = (0,0,0,0)
+		_Screen ("Screen bounds", Vector) = (0, 0, 1280, 720)
+	}
+	SubShader 
+	{
+		Cull Off ZWrite Off ZTest Always
+		Pass {
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#include "UnityCG.cginc"
+		
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+				float4 screenPos : TEXCOORD8;
+			};
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);    
+				o.screenPos = ComputeScreenPos(o.vertex);
+				o.uv = v.uv;
+				return o;
+			}
+
+			uniform sampler2D _MainTex;
+			float4 _Color;
+			float4 _Screen;
+
+			fixed4 frag (v2f i) : SV_Target 
+			{
+				fixed4 base = tex2D(_MainTex, i.uv);
+
+				if (i.screenPos.x <= _Screen.x)
+					return _Color;
+				if (i.screenPos.x >= _Screen.z)
+					return _Color;
+
+				if (i.screenPos.y <= _Screen.y)
+					return _Color;
+				if (i.screenPos.y >= _Screen.w)
+					return _Color;
+
+				return base;
+			}
+			ENDCG
+		}
+	}
+}
