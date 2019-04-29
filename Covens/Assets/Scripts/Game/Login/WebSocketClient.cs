6fd0f22e5d55c8eee4bcd57281a0de58dc5512cf
lucas henriques <lucas@oktagongames.com>
2019-04-29 11:19:30 -0300

    added debug log for non implemented websocket events.

--- Covens/Assets/Scripts/Game/Login/WebSocketClient.cs
+++ Covens/Assets/Scripts/Game/Login/WebSocketClient.cs
@@ -237,7 +237,7 @@ public class WebSocketClient : MonoBehaviour
             if (m_EventActionDictionary.ContainsKey(data.command))
                 m_EventActionDictionary[data.command].Invoke(data);
             else
-                Debug.LogError("command not implemented: " + data.command);
+                Debug.LogError("command not implemented: " + data.command + "\n" + data.json);
         }
         catch (System.Exception e)
         {
