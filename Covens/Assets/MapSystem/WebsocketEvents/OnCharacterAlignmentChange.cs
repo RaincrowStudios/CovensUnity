b970f52b207b372c91823f7114264a63aeb4156b
scarecrow95 <mridul@rainrowgames.com>
2019-04-29 11:25:23 -0400

    added character alignment change

--- Covens/Assets/MapSystem/WebsocketEvents/OnCharacterAlignmentChange.cs
+++ Covens/Assets/MapSystem/WebsocketEvents/OnCharacterAlignmentChange.cs
@@ -4,6 +4,9 @@ public class OnCharacterAlignmentChange : MonoBehaviour
 {
     public static void HandleEvent(WSData data)
     {
-
+        var pd = PlayerDataManager.playerData;
+        pd.currentAlignment = data.currentAlignment;
+        pd.maxAlignment = data.maxAlignment;
+        pd.minAlignment = data.minAlignment;
     }
 }
