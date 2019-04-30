caed977a990629a77e8726635c89fb13286de60d
lucas henriques <lucas@oktagongames.com>
2019-04-30 09:41:47 -0300

    [adjust] buildings height clamped to 50 (unity units)

--- Covens/Assets/CovensMusk/CovensMuskMap.cs
+++ Covens/Assets/CovensMusk/CovensMuskMap.cs
@@ -106,7 +106,7 @@ public class CovensMuskMap : MonoBehaviour
             }.Build(),
             ModeledStructureStyle = new ModeledStructureStyle.Builder
             {
-                BuildingMaterial = new Material(m_WallMaterial)
+                BuildingMaterial = new Material(m_WallMaterial),
             }.Build(),
             RegionStyle = new RegionStyle.Builder
             {
@@ -157,6 +157,7 @@ public class CovensMuskMap : MonoBehaviour
         m_MapsService.Events.MapEvents.Progress.AddListener(OnMapLoadProgress);
         m_MapsService.Events.MapEvents.Loaded.AddListener(OnMapLoaded);
         m_MapsService.Events.ExtrudedStructureEvents.WillCreate.AddListener(OnWillCreateExtrudedStructure);
+        m_MapsService.Events.ExtrudedStructureEvents.DidCreate.AddListener(OnDidCreateExtrudedStructure);
         m_MapsService.Events.ModeledStructureEvents.WillCreate.AddListener(OnWillCreateModeledStructure);
 
         //force layer of spawned objects
@@ -339,6 +340,22 @@ public class CovensMuskMap : MonoBehaviour
     private void OnMapLoadError(MapLoadErrorArgs args)
     {
         Debug.LogError("load map error [" + args.DetailedErrorCode + "] " + args.Message);
+    }
+
+    private void OnDidCreateExtrudedStructure(DidCreateExtrudedStructureArgs e)
+    {
+        if (e.MapFeature.Shape.Extrusions.Length == 0)
+            return;
+
+        float height = e.MapFeature.Shape.Extrusions[0].MaxZ;
+        if (height > 50)
+        {
+            e.GameObject.transform.localScale = new Vector3(1, 50 / height, 1);
+
+#if UNITY_EDITOR
+            e.GameObject.name = "h(" + height + ") " + e.GameObject.name;
+#endif
+        }
     }
 
     private void OnWillCreateExtrudedStructure(WillCreateExtrudedStructureArgs e)
