38e63e12af2c6fb50092d03de2fd94a00dc6744b
lucas henriques <lucas@oktagongames.com>
2019-05-28 08:28:37 -0300

    [wip] chat

--- Covens/Assets/Scripts/Game/Chat/v2/ChatItem/UIChatLocation.cs
+++ Covens/Assets/Scripts/Game/Chat/v2/ChatItem/UIChatLocation.cs
@@ -2,11 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Raincrow.Chat;
+using TMPro;
+using UnityEngine.UI;
 
 public class UIChatLocation : UIChatItem
 {
+    [SerializeField] private Sprite[] m_Avatars;
+
+    [Header("Message")]
+    [SerializeField] private TextMeshProUGUI m_PlayerName;
+    [SerializeField] private TextMeshProUGUI m_PlayerDegree;
+    [SerializeField] private TextMeshProUGUI m_TimeAgo;
+
+    [Header("Buttons")]
+    [SerializeField] private Button m_FlyToButton;
+
     public override void SetupMessage(ChatMessage message)
     {
-        throw new System.NotImplementedException();
+
     }
 }
