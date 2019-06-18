7bffab4762ebd1851c9aa3832cd8d91b68207d03
Emanuel Tavares <emanuel.oktagon@gmail.com>
2019-06-18 11:23:34 -0300

    remove LoadingOverlay when application quits

--- Covens/Assets/MapSystem/LoadingOverlay/LoadingOverlay.cs
+++ Covens/Assets/MapSystem/LoadingOverlay/LoadingOverlay.cs
@@ -4,33 +4,45 @@ using UnityEngine;
 
 public class LoadingOverlay : MonoBehaviour
 {
-    private static LoadingOverlay m_Instance;
-    private static Canvas m_Canvas;
-    private static UnityEngine.UI.GraphicRaycaster m_InputRaycaster;
+    private static LoadingOverlay Instance;
+    private static Canvas Canvas;
+    private static UnityEngine.UI.GraphicRaycaster InputRaycaster;
 
     public static void Show()
     {
-        if (m_Instance == null)
+        if (Instance == null)
+        {
             Instantiate();
+        }
 
-        m_Canvas.enabled = true;
-        m_InputRaycaster.enabled = true;
+        Canvas.enabled = true;
+        InputRaycaster.enabled = true;
     }
 
     public static void Hide()
     {
-        if (m_Instance == null)
+        if (Instance == null)
+        {
             Instantiate();
+        }
 
-        m_Canvas.enabled = false;
-        m_InputRaycaster.enabled = false;
+        Canvas.enabled = false;
+        InputRaycaster.enabled = false;
     }
 
     private static void Instantiate()
     {
         LoadingOverlay prefab = Resources.Load<LoadingOverlay>("LoadingOverlay");
-        m_Instance = GameObject.Instantiate(prefab);
-        m_Canvas = m_Instance.GetComponent<Canvas>();
-        m_InputRaycaster = m_Instance.GetComponent<UnityEngine.UI.GraphicRaycaster>();
+        Instance = Instantiate(prefab);
+        Canvas = Instance.GetComponent<Canvas>();
+        InputRaycaster = Instance.GetComponent<UnityEngine.UI.GraphicRaycaster>();
+    }
+
+    protected virtual void OnApplicationQuit()
+    {
+        if (Instance != null)
+        {
+            DestroyImmediate(Instance);
+        }        
     }
 }
