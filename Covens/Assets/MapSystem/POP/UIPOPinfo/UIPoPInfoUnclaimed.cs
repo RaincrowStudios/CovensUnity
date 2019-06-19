e1357db4c1b9018137b5d61d07f47d3997ee67eb
lucas henriques <lucas@oktagongames.com>
2019-06-19 14:48:58 -0300

    [wip] new UIPopInfo

---
+++ Covens/Assets/MapSystem/POP/UIPOPinfo/UIPoPInfoUnclaimed.cs
@@ -0,0 +1,41 @@
+﻿using Raincrow.Maps;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPopInfoUnclaimed : MonoBehaviour
+{
+    //[SerializeField] private CanvasGroup m_Group;
+
+    //[SerializeField] private TextMeshProUGUI m_Title;
+    //[SerializeField] private TextMeshProUGUI m_DefendedBy;
+    //[SerializeField] private TextMeshProUGUI m_RewardFrequency;
+
+    //[SerializeField] private Button m_EnterBtn;
+    //[SerializeField] private Button m_CloseBtn;
+
+    //private int m_TweenId;
+
+    //private void Awake()
+    //{
+    //    m_Group.gameObject.SetActive(false);
+    //    m_Group.alpha = 0;
+    //}
+
+    //public void Show(IMarker marker, Token data)
+    //{
+
+    //}
+
+    //public void SetupDetails(LocationMarkerDetail data)
+    //{
+
+    //}
+
+    //public void Close()
+    //{
+
+    //}
+}
