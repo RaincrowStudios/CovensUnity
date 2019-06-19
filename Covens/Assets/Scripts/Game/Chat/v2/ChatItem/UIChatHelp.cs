06f3f2158859ac6fb3405dac2525392e4232a844
Emanuel Tavares <emanuel.oktagon@gmail.com>
2019-06-19 17:29:01 -0300

    add image rendering to support chat

--- Covens/Assets/Scripts/Game/Chat/v2/ChatItem/UIChatHelp.cs
+++ Covens/Assets/Scripts/Game/Chat/v2/ChatItem/UIChatHelp.cs
@@ -6,18 +6,96 @@ using UnityEngine.UI;
 namespace Raincrow.Chat.UI
 {
     public class UIChatHelp : UIChatItem
-    {
+    {        
         [SerializeField] private Text text;
+        [SerializeField] private Image image;
+        [SerializeField] private Button imageButton;
+        [SerializeField] private LayoutElement layoutElement;
+        [SerializeField] private float toggleImageScale = 0.65f;
+        [SerializeField] private float preferredHeight = 1080;
         [SerializeField] private TextMeshProUGUI timestamp;
+
+        private bool useSmallImageSize = true;
+
+        protected virtual void OnEnable()
+        {
+            imageButton.onClick.AddListener(ToggleImageSize);
+        }
+
+        protected virtual void OnDisable()
+        {
+            imageButton.onClick.RemoveListener(ToggleImageSize);
+        }
 
         public override void SetupMessage(ChatMessage message,
                                           UnityAction<bool> onRequestChatLoading = null,
                                           UnityAction onRequestChatClose = null)
         {
-            base.SetupMessage(message, onRequestChatLoading, onRequestChatClose);
-            text.text = message.data.message;
+            base.SetupMessage(message, onRequestChatLoading, onRequestChatClose);            
+
+            string messageText = message.data.message;
+            if (!string.IsNullOrEmpty(messageText))
+            {
+                text.text = messageText;
+                text.gameObject.SetActive(true);
+            }            
+            else
+            {
+                text.gameObject.SetActive(false);
+            }
+
+            byte[] imageBytes = message.data.image;
+            if (imageBytes != null && imageBytes.Length > 0)
+            {
+                Texture2D texture = new Texture2D(1, 1);
+                texture.LoadImage(imageBytes);
+                texture.Apply();
+
+                Rect imageRect = new Rect(0, 0, texture.width, texture.height);
+
+                image.overrideSprite = Sprite.Create(texture, imageRect, new Vector2(0.5f, 0.5f));                
+
+                layoutElement.preferredHeight = preferredHeight * toggleImageScale;
+                float widthRatio = layoutElement.preferredHeight / texture.height;
+                float preferredWidth = texture.width * widthRatio;
+                layoutElement.preferredWidth = preferredWidth;
+
+                SetImageSize(true);
+                image.gameObject.SetActive(true);
+            }
+            else
+            {
+                image.gameObject.SetActive(false);
+            }
+
             timestamp.text = Utilities.EpochToDateTimeChat(message.timestamp);
         }
-    }
 
+        private void ToggleImageSize()
+        {
+            SetImageSize(!useSmallImageSize);
+        }
+
+        private void SetImageSize(bool imageBig)
+        {
+            useSmallImageSize = imageBig;
+
+            if (useSmallImageSize)
+            {
+                Texture2D texture = image.overrideSprite.texture;
+                layoutElement.preferredHeight = preferredHeight * toggleImageScale;
+                float widthRatio = layoutElement.preferredHeight / texture.height;
+                float preferredWidth = texture.width * widthRatio;
+                layoutElement.preferredWidth = preferredWidth;
+            }
+            else
+            {
+                Texture2D texture = image.overrideSprite.texture;
+                layoutElement.preferredHeight = preferredHeight;
+                float widthRatio = layoutElement.preferredHeight / texture.height;
+                float preferredWidth = texture.width * widthRatio;
+                layoutElement.preferredWidth = preferredWidth;
+            }
+        }
+    }
 }
