336c1c765ae689ea9e9252bcc797591a06c740f7
lucas henriques <lucas@oktagongames.com>
2019-06-19 09:47:31 -0300

    [added] Close place of power when receiving character_location_boot

--- Covens/Assets/MapSystem/WebsocketEvents/OnCharacterLocationBoot.cs
+++ Covens/Assets/MapSystem/WebsocketEvents/OnCharacterLocationBoot.cs
@@ -5,22 +5,6 @@ public static class OnCharacterLocationBoot
 {
     public static void HandleEvent(WSData data)
     {
-        //PlayerManager.Instance.StartCoroutine(BootCharacterLocation(data));
-        Debug.LogError("TODO:ONCHARACTERLOCATIONBOOT");
+        PlaceOfPower.LeavePoP(false);
     }
-
-    //private static IEnumerator BootCharacterLocation(WSData data, float delay = 0)
-    //{
-    //    yield return new WaitForSeconds(delay);
-    //    var lm = LocationUIManager.Instance;
-    //    if (LocationUIManager.isLocation)
-    //    {
-    //        if (SummoningManager.isOpen)
-    //        {
-    //            SummoningController.Instance.Close();
-    //        }
-    //        lm.Escape();
-    //    }
-    //    yield return null;
-    //}
 }
