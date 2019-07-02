83de4c9ccdfe2123fbdde0f55a6e72a8caba1657
lucas henriques <lucas@oktagongames.com>
2019-07-02 10:15:56 -0300

    fix apple update url

--- Covens/Assets/Scripts/Game/StartUp/StartUpManager.cs
+++ Covens/Assets/Scripts/Game/StartUp/StartUpManager.cs
@@ -291,7 +291,8 @@ public class StartUpManager : MonoBehaviour
 
     public void updateApple()
     {
-        Application.OpenURL("https://testflight.apple.com/join/dusXyBlR");
+        Application.OpenURL("https://apps.apple.com/us/app/covens/id1456181456");
+
     }
 
 }
