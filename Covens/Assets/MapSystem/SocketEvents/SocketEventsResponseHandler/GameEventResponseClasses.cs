92c1c6948327cf466a3873ead8b494ec03318b14
Emanuel Tavares <emanuel.oktagon@gmail.com>
2019-07-15 16:24:11 -0300

    Finish MapMoveResponseHandler

--- Covens/Assets/MapSystem/SocketEvents/SocketEventsResponseHandler/GameEventResponseClasses.cs
+++ Covens/Assets/MapSystem/SocketEvents/SocketEventsResponseHandler/GameEventResponseClasses.cs
@@ -6,14 +6,14 @@ namespace Raincrow.GameEventResponses
     public class SpellCastResponse
     {
         [SerializeField] private string spell;
-        [SerializeField] private Character caster;
-        [SerializeField] private Character target;
+        [SerializeField] private CharacterMarker caster;
+        [SerializeField] private CharacterMarker target;
         [SerializeField] private DamageResult result;
         [SerializeField] private double timestamp;
 
         public string Spell { get => spell; set => spell = value; }
-        public Character Caster { get => caster; set => caster = value; }
-        public Character Target { get => target; set => target = value; }
+        public CharacterMarker Caster { get => caster; set => caster = value; }
+        public CharacterMarker Target { get => target; set => target = value; }
         public DamageResult Result { get => result; set => result = value; }
         public double Timestamp { get => timestamp; set => timestamp = value; }
     }
@@ -31,24 +31,89 @@ namespace Raincrow.GameEventResponses
     }
 
     [System.Serializable]
-    public class Character
+    public class SpiritMarker
     {
         [SerializeField] private string id;
+        [SerializeField] private string spirit;
         [SerializeField] private MarkerSpawner.MarkerType type;
+        [SerializeField] private int baseEnergy;
         [SerializeField] private int energy;
-
-        public string Id { get => id; set => id = value; }
-        public MarkerSpawner.MarkerType Type { get => type; set => type = value; }
-        public int Energy { get => energy; set => energy = value; }
-    }
-    
-    [System.Serializable]
-    public class Coordinate
-    {
         [SerializeField] private float longitude;
         [SerializeField] private float latitude;
 
-        public float Longitude { get => longitude; set => longitude = value; }
-        public float Latitude { get => latitude; set => latitude = value; }
+        public string Id { get => id; set => id = value; }
+        public string Spirit { get => spirit; set => spirit = value; }
+        public MarkerSpawner.MarkerType Type { get => type; set => type = value; }
+        public int BaseEnergy { get => baseEnergy; set => baseEnergy = value; }
+        public int Energy { get => energy; set => energy = value; }
+    }
+
+    [System.Serializable]
+    public class ItemMarker
+    {
+        [SerializeField] private string id;
+        [SerializeField] private string collectible;
+        [SerializeField] private int amount;
+        [SerializeField] private float longitude;
+        [SerializeField] private float latitude;
+        [SerializeField] private IngredientType type;
+
+        public string Id { get => id; }
+        public string Collectible { get => collectible; }
+        public int Amount { get => amount; }
+        public IngredientType Type { get => type; }
+        public float Longitude { get => longitude; }
+        public float Latitude { get => latitude; }
+    }
+
+    [System.Serializable]
+    public class CharacterMarker
+    {
+        [SerializeField] private string id;
+        [SerializeField] private string name;
+        [SerializeField] private MarkerSpawner.MarkerType type;
+        [SerializeField] private int baseEnergy;        
+        [SerializeField] private int energy;
+        [SerializeField] private string state;
+        [SerializeField] private int level;
+        [SerializeField] private Equipment[] equipped;
+        [SerializeField] private string[] immunities;
+        [SerializeField] private int degree;
+        [SerializeField] private float longitude;
+        [SerializeField] private float latitude;
+
+        public string Id { get => id; }
+        public string Name { get => name; }
+        public MarkerSpawner.MarkerType Type { get => type; }
+        public int BaseEnergy { get => baseEnergy; }
+        public int Energy { get => energy; }                
+        public string State { get => state; }
+        public int Level { get => level; }
+        public Equipment[] Equipped { get => equipped; }
+        public string[] Immunities { get => immunities; }
+        public int Degree { get => degree; }
+    }
+
+    public class Equipment
+    {
+        [SerializeField] private string id;
+        [SerializeField] private string[] assets;
+
+        public string Id { get => id; }
+        public string[] Assets { get => assets; }        
+    }
+    
+    [System.Serializable]
+    public class Location
+    {
+        [SerializeField] private string dominion;
+        [SerializeField] private string garden;        
+        [SerializeField] private int zone;
+        [SerializeField] private int music;
+
+        public string Dominion { get => dominion; }
+        public string Garden { get => garden; }
+        public int Zone { get => zone; }
+        public int Music { get => music; }
     }
 }
---
+++ Covens/Assets/MapSystem/SocketEvents/SocketEventsResponseHandler/MapMoveResponseHandler.cs
@@ -0,0 +1,28 @@
+﻿using Raincrow.GameEventResponses;
+using UnityEngine;
+
+namespace Raincrow.GameEventResponses
+{
+    public class MapMoveResponseHandler : IGameEventResponseHandler
+    {
+        public void HandleResponse(string eventData)
+        {
+            MapMoveResponse data = JsonUtility.FromJson<MapMoveResponse>(eventData);
+            MarkerManagerAPI.HandleMarkersCallbackOnSuccess(data);
+        }       
+    }
+
+    [System.Serializable]
+    public class MapMoveResponse
+    {
+        [SerializeField] private CharacterMarker[] characters;
+        [SerializeField] private SpiritMarker[] spirits;
+        [SerializeField] private ItemMarker[] items;
+        [SerializeField] private Location location;
+
+        public CharacterMarker[] Characters { get => characters; }
+        public SpiritMarker[] Spirits { get => spirits; }
+        public ItemMarker[] Items { get => items; }
+        public Location Location { get => location; set => location = value; }
+    }
+}
