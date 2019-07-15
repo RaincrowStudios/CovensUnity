fb590bedba4f7c2e10619aaad1172cff726870e1
ocpaynter4 <47328047+ocpaynter4@users.noreply.github.com>
2019-07-15 09:17:05 -0400

    UI Visual changes to Spirit/Physical form popups, UI Visual changes to FlightVisuals, rewrote script for FadeOutDelay

--- Covens/Assets/Scripts/Utility/FadeOutDelay.cs
+++ Covens/Assets/Scripts/Utility/FadeOutDelay.cs
@@ -1,21 +1,28 @@
-﻿using System.Collections; 
-using System.Collections.Generic; 
-using UnityEngine; 
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 [RequireComponent(typeof(CanvasGroup))]
-public class FadeOutDelay : MonoBehaviour { 
+public class FadeOutDelay : MonoBehaviour
+{
 
-	public float fadeDelay = 5;
+    public float fadeDelay = 5;
+    public float fadeSpeed = 1;
 
-	IEnumerator Start(){
-		yield return new WaitForSeconds (fadeDelay);
-		CanvasGroup cg = GetComponent<CanvasGroup> ();
-		float t = 0;
-		while (t <= 1) {
-			t += Time.deltaTime;
-			cg.alpha = Mathf.SmoothStep (1, 0, t);
-			yield return 0;
-		}
-	}
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(fadeDelay);
+        CanvasGroup cg = GetComponent<CanvasGroup>();
+        LeanTween.value(0f, 1f, fadeDelay).setOnComplete(() =>
+        {
+            LeanTween.alphaCanvas(cg, 0f, fadeSpeed);
+        });
+        /*		float t = 0;
+                while (t <= 1) {
+                    t += Time.deltaTime;
+                    cg.alpha = Mathf.SmoothStep (1, 0, t);
+                    yield return 0;
+                }*/
+    }
 }
 
