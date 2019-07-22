e9c8e0b0711dda7df87b24681c80abfe3e5b94b8
ocpaynter4 <47328047+ocpaynter4@users.noreply.github.com>
2019-07-22 11:41:40 -0400

    Moved old kyteler ring stuff, added new Prefab to go into character UI of BoS

---
+++ Covens/Assets/Scripts/Game/Kyteler/UIBoSKytelerManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class UIBoSKytelerManager : MonoBehaviour
+{
+    public Button openInfo;
+    public Button back;
+    public GameObject RingInfo;
+    public float closedXLocal = 2160f;
+    public float openXLocal = -65f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        openInfo.onClick.AddListener(AnimOpen);
+        back.onClick.AddListener(AnimClose);
+    }
+
+    // Update is called once per frame
+    public void AnimOpen()
+    {
+        LeanTween.moveLocalX(RingInfo, openXLocal, 0.8f).setEase(LeanTweenType.easeInCubic);
+    }
+    public void AnimClose()
+    {
+        LeanTween.moveLocalX(RingInfo, closedXLocal, 0.5f).setEase(LeanTweenType.easeOutCubic);
+    }
+}
