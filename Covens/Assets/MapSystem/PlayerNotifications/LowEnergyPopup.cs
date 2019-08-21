5d466641a9e2a1a4da6e22878a2a842fd0a2b346
ocpaynter4 <47328047+ocpaynter4@users.noreply.github.com>
2019-08-21 11:57:44 -0400

    Updated visuals and setup function for LowEnergyPopup. Needs functionality to take gold from player and heal them

--- Covens/Assets/MapSystem/PlayerNotifications/LowEnergyPopup.cs
+++ Covens/Assets/MapSystem/PlayerNotifications/LowEnergyPopup.cs
@@ -11,29 +11,37 @@ public class LowEnergyPopup : MonoBehaviour
     [SerializeField] private TextMeshProUGUI m_offerText;
     [SerializeField] private TextMeshProUGUI m_buttonText;
     [SerializeField] private Button m_close;
+    [SerializeField] private Button m_continue;
+    [SerializeField] private CanvasGroup thisCG;
 
     private void Awake()
     {
         Instance = this;
+        thisCG = this.GetComponent<CanvasGroup>();
+        LeanTween.alphaCanvas(thisCG, 1f, 0.5f).setEaseInCubic();
+
     }
 
     // Start is called before the first frame update
     void Start()
     {
         m_offerText.text = LocalizeLookUp.GetText("energy_restore_message");
-        m_close.onClick.AddListener(Close);
+        m_close.onClick.AddListener(() => { Close(); });
+
         //this will need localization
         if (PlayerDataManager.playerData.gold < 1)
         {
             m_buttonText.text = string.Concat("<color=red>", LocalizeLookUp.GetText("energy_restore_missing"), "</color>");
-            m_buttonText.transform.GetChild(0).GetComponent<Button>().onClick.AddListener(() => {
+            m_continue.onClick.AddListener(() =>
+            {
                 ShopManager.OpenSilverStore();
             });
         }
         else
         {
             m_buttonText.text = LocalizeLookUp.GetText("energy_restore_offer");
-            m_buttonText.transform.GetChild(0).GetComponent<Button>().onClick.AddListener(() => {
+            m_continue.onClick.AddListener(() =>
+            {
                 SetupConfirmation();
             });
         }
@@ -44,7 +52,8 @@ public class LowEnergyPopup : MonoBehaviour
     {
         m_offerText.text = LocalizeLookUp.GetText("energy_restore_confirm");
         m_buttonText.text = LocalizeLookUp.GetText("store_accept");
-        m_buttonText.transform.GetChild(0).GetComponent<Button>().onClick.AddListener(() => {
+        m_continue.onClick.AddListener(() =>
+        {
             AddEnergy();
         });
     }
@@ -53,7 +62,7 @@ public class LowEnergyPopup : MonoBehaviour
     void AddEnergy()
     {
 
-        var pData = PlayerDataManager.playerData;        
+        var pData = PlayerDataManager.playerData;
         pData.gold--;
         PlayerManagerUI.Instance.UpdateDrachs();
         pData.energy = pData.baseEnergy;
@@ -73,6 +82,10 @@ public class LowEnergyPopup : MonoBehaviour
 
     void Close()
     {
-        Destroy(gameObject);
+        Debug.Log("Closing");
+        LeanTween.alphaCanvas(thisCG, 0f, 0.4f).setOnComplete(() =>
+        {
+            Destroy(gameObject);
+        }).setEaseOutCubic();
     }
 }
