74bf0c4439257e826c5626eb36d06449a6bcf654
lucas henriques <lucas@oktagongames.com>
2019-08-22 17:04:22 -0300

    add palceholder character.state handler

---
+++ Covens/Assets/MapSystem/SocketEvents/SocketEventsResponseHandler/CharacterStateHandler.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using Raincrow.Maps;
+using UnityEngine;
+
+namespace Raincrow.GameEventResponses
+{
+    public class CharacterStateHandler : IGameEventHandler
+    {
+        public string EventName => "character.state";
+
+        public void HandleResponse(string eventData)
+        {
+            Debug.LogError("TODO: HANDLE CHARACTER.STATE");
+        }
+    }
+}
