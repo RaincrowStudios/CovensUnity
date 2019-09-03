6f2c805dd5731c647580d917745b0fae7dd205ea
lucas henriques <lucas@oktagongames.com>
2019-09-03 15:01:36 -0300

    remove LeanTween.cancelAll from UIDominionSplash

--- Covens/Assets/Scripts/Game/StartUp/UIDominionSplash.cs
+++ Covens/Assets/Scripts/Game/StartUp/UIDominionSplash.cs
@@ -17,20 +17,23 @@ public class UIDominionSplash : MonoBehaviour
     public static UIDominionSplash Instance { get; private set; }
 
     private System.Action m_OnClose;
+    private int m_TimerTweenId;
 
     private void Awake()
     {
+
+        Instance = this;
+        m_CloseButton.gameObject.SetActive(false);
+        m_CloseButton.onClick.AddListener(Close);
+
         if (FTFManager.InFTF)
         {
             m_Canvas.enabled = false;
-            Destroy(this.gameObject);
+            m_InputRaycaster.enabled = false;
+            m_CanvasGroup.alpha = 0;
         }
         else
         {
-            Instance = this;
-            m_CloseButton.gameObject.SetActive(false);
-            m_CloseButton.onClick.AddListener(Close);
-
             m_InputRaycaster.enabled = true;
             m_Canvas.enabled = true;
             m_CanvasGroup.alpha = 1;
@@ -39,6 +42,10 @@ public class UIDominionSplash : MonoBehaviour
 
     public void Show(System.Action onClose)
     {
+        m_Canvas.enabled = true;
+        m_InputRaycaster.enabled = true;
+        LeanTween.alphaCanvas(m_CanvasGroup, 1, 1f).setEaseOutCubic();
+
         m_CloseButton.gameObject.SetActive(true);
         m_OnClose = onClose;
         m_Dominion.text = m_TopWitch.text = m_TopCoven.text = "";
@@ -46,8 +53,7 @@ public class UIDominionSplash : MonoBehaviour
         float time = 2.5f;
         m_Dominion.text = LocalizeLookUp.GetText("dominion_location") + " " + GameStartup.Dominion;
         LeanTween.value(0, 1f, 2f).setOnUpdate((float a) => m_Dominion.alpha = a).setDelay(0.25f).setEaseOutCubic();
-        //Debug.Log("Top Player" + GameStartup.TopPlayer);
-        //Debug.Log("Top Coven" + GameStartup.TopCoven);
+
         if (string.IsNullOrEmpty(GameStartup.TopPlayer) == false)
         {
             m_TopWitch.gameObject.SetActive(true);
@@ -71,18 +77,17 @@ public class UIDominionSplash : MonoBehaviour
         {
             m_TopCoven.gameObject.SetActive(false);
         }
-        LeanTween.value(0f, 1f, time).setOnComplete(() =>
-          {
-              Close();
-          });
+        m_TimerTweenId = LeanTween.value(0f, 0f, 0).setDelay(time).setOnStart(Close).uniqueId;
     }
 
     private void Close()
     {
+        LeanTween.cancel(m_TimerTweenId);
+        m_InputRaycaster.enabled = false;
+
         m_OnClose?.Invoke();
         m_OnClose = null;
-        LeanTween.cancelAll();
-        m_InputRaycaster.enabled = false;
+
         LeanTween.alphaCanvas(m_CanvasGroup, 0, 1f).setEaseOutCubic().setOnComplete(() =>
         {
             m_Canvas.enabled = false;
