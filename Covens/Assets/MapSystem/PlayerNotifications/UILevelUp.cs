c7428c04153af36775b4356eda248fc65524d108
lucas henriques <lucas@oktagongames.com>
2019-09-04 17:05:46 -0300

    add unlockedSpell to Levelup popup

--- Covens/Assets/MapSystem/PlayerNotifications/UILevelUp.cs
+++ Covens/Assets/MapSystem/PlayerNotifications/UILevelUp.cs
@@ -6,12 +6,13 @@ using TMPro;
 
 public class UILevelUp : MonoBehaviour
 {
-    [SerializeField] private GameObject m_Content;
     [SerializeField] private CanvasGroup m_CanvasGroup;
-    [SerializeField] private TextMeshProUGUI m_Title;
-    [SerializeField] private TextMeshProUGUI m_Description;
+    [SerializeField] private TextMeshProUGUI m_LevelText;
     [SerializeField] private Button m_CloseButton;
-    [SerializeField] private Animator m_Animator;
+    [Space(10)]
+    [SerializeField] private LayoutGroup m_RewardContainer;
+    [SerializeField] private UILevelUpReward m_RewardPrefab;
+
 
     private static UILevelUp m_Instance;
     public static UILevelUp Instance
@@ -24,44 +25,83 @@ public class UILevelUp : MonoBehaviour
         }
     }
 
-    private int m_TweenId;
-
     private void Awake()
     {
         m_CloseButton.onClick.AddListener(Close);
-        m_Content.SetActive(false);
+        m_CanvasGroup.alpha = 0;
+        m_CloseButton.interactable = false;
+        m_RewardPrefab.gameObject.SetActive(false);
     }
 
     public void Show()
     {
-        m_Title.text = "You Leveled up!";
-        m_Description.text = "Level " + PlayerDataManager.playerData.level + "!";
+        m_LevelText.text = LocalizeLookUp.GetText("leveled_number") + ": " + PlayerDataManager.playerData.level;
 
-        m_Animator.enabled = true;
-        m_Content.SetActive(true);
+        StartCoroutine(SpawnRewards());
 
-        m_CloseButton.interactable = false;
-        LeanTween.value(0, 0, 0).setDelay(0.1f).setOnStart(() => { m_CloseButton.interactable = true; });
         SoundManagerOneShot.Instance.PlayLevel();
+        
+        m_CloseButton.interactable = false;
+        LeanTween.value(0, 0, 0).setDelay(0.25f).setOnStart(() => { m_CloseButton.interactable = true; });
+
+        LeanTween.alphaCanvas(m_CanvasGroup, 1, 0.7f)
+            .setEaseOutCubic();
+    }
+
+    private IEnumerator SpawnRewards()
+    {
+        List<SpellData> unlockedSpells = new List<SpellData>();
+        List<UILevelUpReward> rewardItems = new List<UILevelUpReward>();
+        foreach (var spell in PlayerDataManager.playerData.UnlockedSpells)
+        {
+            if (spell.level == PlayerDataManager.playerData.level)
+            {
+                unlockedSpells.Add(spell);
+                rewardItems.Add(Instantiate(m_RewardPrefab, m_RewardContainer.transform));
+                rewardItems[rewardItems.Count - 1].gameObject.SetActive(true);
+            }
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        int left = unlockedSpells.Count / 2-1;
+        int right = left + 1;
+
+        //center first
+        if (unlockedSpells.Count % 2 == 1)
+        {
+            rewardItems[right].SetupSpell(unlockedSpells[right].id);
+            right++;
+            yield return new WaitForSeconds(0.45f);
+        }
+
+        //setup from center to borders
+        while (left >= 0 || right < unlockedSpells.Count)
+        {
+            if (left >= 0)
+            {
+                rewardItems[left].SetupSpell(unlockedSpells[left].id);
+                left--;
+            }
+            if (right < unlockedSpells.Count)
+            {
+                rewardItems[right].SetupSpell(unlockedSpells[right].id);
+                right++;
+            }
+            yield return new WaitForSeconds(0.5f);
+        }
     }
 
     private void Close()
     {
-        m_Animator.enabled = false;
         m_CloseButton.interactable = false;
 
-        LeanTween.cancel(m_TweenId);
-        m_TweenId = LeanTween.value(m_CanvasGroup.alpha, 0, 0.4f)
+        LeanTween.alphaCanvas(m_CanvasGroup, 0, 0.4f)
             .setEaseOutCubic()
-            .setOnUpdate((float t) =>
-            {
-                m_Content.transform.localScale = new Vector3(t, t, t);
-                m_CanvasGroup.alpha = t;
-            })
             .setOnComplete(() =>
             {
+                gameObject.SetActive(false);
                 Destroy(this.gameObject);
-            })
-            .uniqueId;
+            });
     }
 }
---
+++ Covens/Assets/MapSystem/PlayerNotifications/UILevelUpReward.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UILevelUpReward : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup m_CanvasGroup;
+    [SerializeField] private TextMeshProUGUI m_Title;
+    [SerializeField] private TextMeshProUGUI m_Subtitle;
+
+    [SerializeField] private Image m_Art;
+
+    [Space(10)]
+    [SerializeField] private Sprite m_SilverDrachsArt;
+    
+    private void Awake()
+    {
+        m_CanvasGroup.alpha = 0;
+    }
+
+    public void SetupSilver(int amount)
+    {
+        m_Title.text = LocalizeLookUp.GetText("ftf_reward_upper");
+        m_Subtitle.text = LocalizeLookUp.GetText("store_bought_drachs_upper").Replace("{{Number}}", amount.ToString());
+        m_Art.overrideSprite = m_SilverDrachsArt;
+        Show();
+    }
+
+    public void SetupSpell(string id)
+    {
+        m_Title.text = LocalizeLookUp.GetText("store_gear_unlocked_upper");
+        m_Subtitle.text = LocalizeLookUp.GetSpellName(id);
+
+        m_Art.color = new Color(1, 1, 1, 0);
+        DownloadedAssets.GetSprite(id, (spr) =>
+        {
+            m_Art.overrideSprite = spr;
+            LeanTween.color(m_Art.rectTransform, Color.white, 1f);
+        });
+        Show();
+    }
+
+    private void Show()
+    {
+        gameObject.SetActive(true);
+        LeanTween.alphaCanvas(m_CanvasGroup, 1, 1);
+    }
+}
