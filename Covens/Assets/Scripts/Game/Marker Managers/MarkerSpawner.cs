228abbc21cc0e0be2e0ff2f7bfc5ab3a2e67175b
lucas henriques <lucas@oktagongames.com>
2019-09-10 16:47:17 -0300

    fix spirits not following small distances

--- Covens/Assets/Scripts/Game/Marker Managers/MarkerSpawner.cs
+++ Covens/Assets/Scripts/Game/Marker Managers/MarkerSpawner.cs
@@ -120,6 +120,7 @@ public class MarkerSpawner : MarkerManager
             {
                 item.Coords = new Vector2(Data.longitude, Data.latitude);
                 item.Setup(Data);
+                item.SetWorldPosition(MapsAPI.Instance.GetWorldPosition(Data.longitude, Data.latitude), 1);
                 UpdateMarker(item);
             }
             return Markers[Data.instance][0];
