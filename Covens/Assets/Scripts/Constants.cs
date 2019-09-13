0fe8870a122fe4330410b502db4727cf3fb01fca
lucas henriques <lucas@oktagongames.com>
2019-09-13 14:43:16 -0300

    update staging address

--- Covens/Assets/Scripts/Constants.cs
+++ Covens/Assets/Scripts/Constants.cs
@@ -33,12 +33,12 @@ public class CovenConstants : MonoBehaviour
             }
             else
             {
-                return "http://34.73.145.51:9000/api/";
+                return "https://game-server.raincrow.pw/api/";
             }
 #elif PRODUCTION
            return "https://game-server-production-dot-covens.appspot.com/api/";
 #else
-            return "http://34.73.145.51:9000/api/";
+            return "https://game-server.raincrow.pw/api/";
 #endif
         }
     }
@@ -77,12 +77,12 @@ public class CovenConstants : MonoBehaviour
             }
             else
             {
-                return "http://34.73.145.51:8084/socket.io/?";
+                return "https://comms-server.raincrow.pw/socket.io/";
             }
 #elif PRODUCTION
                 return "http://35.186.184.246:8084/socket.io/";
 #else
-            return "http://34.73.145.51:8084/socket.io/?";
+            return "https://comms-server.raincrow.pw/socket.io/?";
 #endif
         }
     }
