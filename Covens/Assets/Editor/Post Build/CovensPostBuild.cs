b91deb60e03f5afaf2eb15cc3fe95437bd1e06ae
lucas henriques <lucas@oktagongames.com>
2019-09-25 17:19:44 -0300

    add removal of "UIApplicationExitsOnSuspend" to Covens post process build

--- Covens/Assets/Editor/Post Build/CovensPostBuild.cs
+++ Covens/Assets/Editor/Post Build/CovensPostBuild.cs
@@ -20,9 +20,20 @@ public class CovensPostBuild : MonoBehaviour
             PlistElementDict rootDict = plist.root;
             rootDict.SetString("NSLocationAlwaysUsageDescription", "Covens uses your real location data in order to play the game.");
 
+            if (rootDict.values.ContainsKey("UIApplicationExitsOnSuspend"))
+            {
+                Log("removing \"UIApplicationExitsOnSuspend\" from \"" + plistPath + "\"");
+                rootDict.values.Remove("UIApplicationExitsOnSuspend");
+            }
+
             // Write plist
             System.IO.File.WriteAllText(plistPath, plist.WriteToString());
         }        
+    }
+
+    private static void Log(string msg)
+    {
+        Debug.Log("[CovensPostBuild] " + msg);
     }
 }
 #endif
