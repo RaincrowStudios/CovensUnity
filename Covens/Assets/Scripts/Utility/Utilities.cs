02196ebd4ad4c8183b2a8a4bb3a2f7e67ca1a2d5
scarecrow95 <mridul@rainrowgames.com>
2019-09-26 10:48:54 -0400

    added FTFpop

--- Covens/Assets/Scripts/Utility/Utilities.cs
+++ Covens/Assets/Scripts/Utility/Utilities.cs
@@ -589,6 +589,21 @@ public static class ArrayExtensions
         }
         return false;
     }
+
+    private static System.Random rng = new System.Random();
+
+    public static void Shuffle<T>(this IList<T> list)
+    {
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = rng.Next(n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
 }
 
 public static class JsonExtensions
