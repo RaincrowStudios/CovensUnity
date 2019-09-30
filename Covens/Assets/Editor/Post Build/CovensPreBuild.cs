dc059e08af369393cbb0cbc43454b4aa98f7383a
lucas henriques <lucas@oktagongames.com>
2019-09-30 09:11:59 -0300

    testing preprocess sctipt to automaticaly increase build number

---
+++ Covens/Assets/Editor/Post Build/CovensPreBuild.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+using UnityEngine;
+
+public class CovensPreBuild : MonoBehaviour
+{
+#if UNITY_CLOUD_BUILD
+		public static void PreBuildUCB(UnityEngine.CloudBuild.BuildManifestObject manifest)
+        {
+            int iBuildNumber = 0;
+            int.TryParse(manifest.GetValue<string>("buildNumber"), out iBuildNumber);
+            iBuildNumber += 1000;
+#if UNITY_ANDROID
+            PlayerSettings.Android.bundleVersionCode = iBuildNumber;
+#elif UNITY_IOS
+            PlayerSettings.iOS.buildNumber = iBuildNumber.ToString();
+#endif
+        }
+#endif
+}
