14dabc95ec9cd904de9a55d11f01a390006a9041
lucas henriques <lucas@oktagongames.com>
2019-10-01 09:10:14 -0300

    wip store UI

--- Covens/Assets/Scripts/Game/Shop/UIStore.cs
+++ Covens/Assets/Scripts/Game/Shop/UIStore.cs
@@ -3,19 +3,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UIStore : MonoBehaviour
 {
+    public enum Screen
+    {
+        NONE = 0,
+        HOME,
+        COSMETICS,
+        STYLES,
+        CURRENCY,
+        INGREDIENTS,
+        CHARMS,
+    }
+
     [SerializeField] private Canvas m_Canvas;
     [SerializeField] private GraphicRaycaster m_InputRaycaster;
     [SerializeField] private CanvasGroup m_CanvasGroup;
 
     [Space()]
-    [SerializeField] private HorizontalLayoutGroup m_Header;
+    [SerializeField] private RectTransform m_Header;
     [SerializeField] private Button m_CloseButton;
 
     [Header("Home")]
     [SerializeField] private CanvasGroup m_HomeWindow;
+    [SerializeField] private Button m_CosmeticsButton;
+    [SerializeField] private Button m_CurrenciesButton;
+    [SerializeField] private Button m_CharmsButton;
+    [SerializeField] private Button m_IngredientsButton;
 
     [Header("Store")]
     [SerializeField] private CanvasGroup m_StoreWindow;
@@ -23,9 +39,10 @@ public class UIStore : MonoBehaviour
     [SerializeField] private ScrollRect m_PageScrollView;
     [SerializeField] private UIStorePage m_PagePrefab;
     [SerializeField] private UIStoreItem m_ItemPrefab;
+    [SerializeField] private RectTransform m_BottomBar;
 
     [Header("Styles")]
-    [SerializeField] private CanvasGroup m_StylesWindow;
+    [SerializeField] private UIStoreStylesWindow m_StylesWindow;
 
     private static UIStore m_Instance;
     
@@ -54,13 +71,17 @@ public class UIStore : MonoBehaviour
         }
     }
 
+    private Screen m_CurrentScreen = Screen.NONE;
     private int m_CurrentPage = 0;
+    private float m_PageSize;
     private RectTransform m_CanvasRectTransform;
     private RectTransform m_ContainerRectTransform;
-    private float m_PageSize;
 
     private SimplePool<UIStorePage> m_PagePool;
     private SimplePool<UIStoreItem> m_ItemPool;
+
+    private int m_MainTweenId;
+    private int m_ScreenTweenId;
 
     private void Awake()
     {
@@ -79,10 +100,15 @@ public class UIStore : MonoBehaviour
         m_StylesWindow.alpha = 0;
         m_HomeWindow.alpha = 0;
 
-        m_CloseButton.onClick.AddListener(OnClickClose);
 
         m_PagePool = new SimplePool<UIStorePage>(m_PagePrefab, 3);
         m_ItemPool = new SimplePool<UIStoreItem>(m_ItemPrefab, 20);
+
+        m_CloseButton.onClick.AddListener(OnClickClose);
+        m_CosmeticsButton.onClick.AddListener(() => SetScreen(Screen.COSMETICS));
+        m_CurrenciesButton.onClick.AddListener(() => SetScreen(Screen.CURRENCY));
+        m_CharmsButton.onClick.AddListener(() => SetScreen(Screen.CHARMS));
+        m_IngredientsButton.onClick.AddListener(() => SetScreen(Screen.INGREDIENTS));
     }
 
     private void Start()
@@ -90,6 +116,7 @@ public class UIStore : MonoBehaviour
         m_CanvasRectTransform = m_Canvas.GetComponent<RectTransform>();
         m_ContainerRectTransform = m_PageContainer.GetComponent<RectTransform>();
         m_PageSize = m_CanvasRectTransform.sizeDelta.x;
+        SetScreen(Screen.HOME);
     }
 
     private void Update()
@@ -111,18 +138,268 @@ public class UIStore : MonoBehaviour
 
     private void OnClickClose()
     {
-        Close();
+        if (m_CurrentScreen == Screen.HOME)
+            Close();
+        else
+            SetScreen(Screen.HOME);
     }
 
     [ContextMenu("Open")]
     public void Open()
     {
+        LeanTween.cancel(m_MainTweenId);
+        m_Canvas.enabled = true;
+        m_InputRaycaster.enabled = true;
 
+        m_MainTweenId = LeanTween.value(m_CanvasGroup.alpha, 1, 0.25f)
+            .setOnUpdate((float v) =>
+            {
+                m_CanvasGroup.alpha = v;
+            })
+            .setEaseOutCubic()
+            .uniqueId;
     }
 
     [ContextMenu("Close")]
     public void Close()
     {
+        LeanTween.cancel(m_MainTweenId);
+        m_InputRaycaster.enabled = false;
 
+        m_MainTweenId = LeanTween.value(m_CanvasGroup.alpha, 0, 0.5f)
+            .setOnUpdate((float v) =>
+            {
+                m_CanvasGroup.alpha = v;
+            })
+            .setOnComplete(() =>
+            {
+                m_Canvas.enabled = false;
+            })
+            .setEaseOutCubic()
+            .uniqueId;
+    }
+
+    public void SetScreen(Screen screen)
+    {
+        if (screen == m_CurrentScreen)
+            return;
+
+        LeanTween.cancel(m_ScreenTweenId);
+        CanvasGroup toShow = null;
+        CanvasGroup toHide = null;
+        List<CanvasGroup> screens = new List<CanvasGroup>() { m_HomeWindow, m_StoreWindow, m_StylesWindow.canvasGroup };
+
+        switch (m_CurrentScreen)
+        {
+            case Screen.HOME:           toHide = m_HomeWindow;      break;
+            case Screen.COSMETICS:      toHide = m_StoreWindow;     break;
+            case Screen.CURRENCY:       toHide = m_StoreWindow;     break;
+            case Screen.INGREDIENTS:    toHide = m_StoreWindow;     break;
+            case Screen.STYLES:         toHide = m_StylesWindow.canvasGroup;    break;
+            case Screen.CHARMS:         toShow = m_StoreWindow;     break;
+        }
+        switch (screen)
+        {
+            case Screen.HOME:       toShow = m_HomeWindow; break;
+            case Screen.COSMETICS:  toShow = m_StoreWindow; break;
+            case Screen.CURRENCY:   toShow = m_StoreWindow; break;
+            case Screen.INGREDIENTS:toShow = m_StoreWindow; break;
+            case Screen.STYLES:     toShow = m_StylesWindow.canvasGroup; break;
+            case Screen.CHARMS:     toShow = m_StoreWindow; break;
+        }
+        System.Action setupScreen = () =>
+        {
+            switch (screen)
+            {
+                case Screen.HOME:       SetupHome(); break;
+                case Screen.COSMETICS:  SetupCosmetics(); break;
+                case Screen.CURRENCY:   SetupCurrency(); break;
+                case Screen.INGREDIENTS:SetupIngredients(); break;
+                case Screen.STYLES:     SetupStyles(); break;
+                case Screen.CHARMS:     SetupCharms(); break;
+            }
+        };
+
+        m_CurrentScreen = screen;
+
+        //show and hide at same time
+        if (toShow != toHide)
+        {
+            if (toHide)
+                toHide.blocksRaycasts = toHide.interactable = false;
+            if (toShow)
+                toShow.blocksRaycasts = toShow.interactable = true;
+
+            float start = toShow ? toShow.alpha : 0;
+            m_ScreenTweenId = LeanTween.value(start, 1, 0.5f)
+                .setEaseOutCubic()
+                .setOnStart(() => 
+                {
+                    setupScreen();
+                    toShow?.gameObject.SetActive(true);
+                })
+                .setOnUpdate((float v) =>
+                {
+                    foreach(var item in screens)
+                    {
+                        if (item != toShow)
+                            item.alpha = 1 - v;
+                        else
+                            item.alpha = v;
+                    }
+                })
+                .setOnComplete(() =>
+                {
+                    toHide?.gameObject.SetActive(false);
+                    foreach (var item in screens)
+                    {
+                        if (item != toShow)
+                            item.gameObject.SetActive(false);
+                    }
+                })
+                .uniqueId;
+        }
+        //hide first then show
+        else
+        {
+            if (toHide)
+                toHide.blocksRaycasts = toHide.interactable = false;
+
+            m_ScreenTweenId = LeanTween.alphaCanvas(toHide, 0, 0.2f)
+                .setOnComplete(() =>
+                {
+                    setupScreen();
+
+                    if (toShow)
+                        toShow.blocksRaycasts = toShow.interactable = true;
+
+                    toHide?.gameObject.SetActive(false);
+                    toShow?.gameObject.SetActive(true);
+
+                    m_ScreenTweenId = LeanTween.alphaCanvas(toShow, 1f, 0.5f)
+                        .setEaseOutCubic()
+                        .uniqueId;
+                })
+                .uniqueId;
+        }
+    }
+
+    public void SetHeaderText(params string[] title)
+    {
+        for (int i = 0; i < m_Header.transform.childCount; i++)
+        {
+            if (i < title.Length)
+            {
+                m_Header.transform.GetChild(i).GetComponent<TextMeshProUGUI>().text = title[i];
+                m_Header.transform.GetChild(i).gameObject.SetActive(true);
+            }
+            else
+            {
+                m_Header.transform.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void SetHeaderButtons(params UnityEngine.Events.UnityAction[] onClick)
+    {
+        for (int i = 0; i < m_Header.childCount; i++)
+        {
+            if (i < onClick.Length)
+            {
+                Button button = m_Header.GetChild(i).GetComponent<Button>();
+                button.onClick.RemoveAllListeners();
+                button.onClick.AddListener(onClick[i]);
+                m_Header.GetChild(i).gameObject.SetActive(true);
+            }
+            else
+            {
+                m_Header.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void SetBottomText(params string[] title)
+    {
+        for (int i = 0; i < m_BottomBar.childCount; i++)
+        {
+            if (i < title.Length)
+            {
+                m_BottomBar.GetChild(i).GetComponent<TextMeshProUGUI>().text = title[i];
+                m_BottomBar.GetChild(i).gameObject.SetActive(true);
+            }
+            else
+            {
+                m_BottomBar.transform.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void SetBottomButtons(params UnityEngine.Events.UnityAction[] onClick)
+    {
+        for (int i = 0; i < m_BottomBar.childCount; i++)
+        {
+            if (i < onClick.Length)
+            {
+                Button button = m_BottomBar.GetChild(i).GetComponent<Button>();
+                button.onClick.RemoveAllListeners();
+                button.onClick.AddListener(onClick[i]);
+                m_BottomBar.GetChild(i).gameObject.SetActive(true);
+            }
+            else
+            {
+                m_BottomBar.transform.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void SetupHome()
+    {
+        SetHeaderText();
+    }
+
+    private void SetupCosmetics()
+    {
+        SetHeaderText(
+            LocalizeLookUp.GetText("store_cosmetics"), 
+            LocalizeLookUp.GetText("store_styles"));
+        SetHeaderButtons(
+            () => SetScreen(Screen.COSMETICS),
+            () => SetScreen(Screen.STYLES));
+
+        SetBottomText(
+            LocalizeLookUp.GetText("store_gear_clothing"),
+            LocalizeLookUp.GetText("store_gear_accessories"),
+            LocalizeLookUp.GetText("store_gear_skin_art"),
+            LocalizeLookUp.GetText("store_gear_hairstyle"));
+    }
+
+    private void SetupStyles()
+    {
+        SetHeaderText(
+            LocalizeLookUp.GetText("store_cosmetics"),
+            LocalizeLookUp.GetText("store_styles"));
+        SetHeaderButtons(
+            () => SetScreen(Screen.COSMETICS),
+            () => SetScreen(Screen.STYLES));
+
+        SetBottomText();
+    }
+
+    private void SetupCurrency()
+    {
+        SetHeaderText();
+        SetBottomText();
+    }
+
+    private void SetupCharms()
+    {
+        SetHeaderText();
+        SetBottomText();
+    }
+
+    private void SetupIngredients()
+    {
+        SetHeaderText();
+        SetBottomText();
     }
 }
---
+++ Covens/Assets/Scripts/Game/Shop/UIStoreStylesWindow.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Raincrow.Store;
+
+[RequireComponent(typeof(SwipeDetector)), RequireComponent(typeof(CanvasGroup))]
+public class UIStoreStylesWindow : MonoBehaviour
+{
+    private SwipeDetector m_SwipeDetector;
+    int m_CurrentIndex;
+
+    public CanvasGroup canvasGroup
+    {
+        get;
+        private set;
+    }
+
+    public RectTransform rectTransform
+    {
+        get;
+        private set;
+    }
+
+    public float alpha
+    {
+        get => canvasGroup.alpha;
+        set => canvasGroup.alpha = value;
+    }
+
+    private void Awake()
+    {
+        canvasGroup = this.GetComponent<CanvasGroup>();
+        m_SwipeDetector = this.GetComponent<SwipeDetector>();
+        rectTransform = this.GetComponent<RectTransform>();
+
+        gameObject.SetActive(false);
+        alpha = 0;
+
+        m_SwipeDetector.SwipeLeft = OnSwipeLeft;
+        m_SwipeDetector.SwipeRight = OnSwipeRight;
+    }
+
+    private void OnSwipeLeft()
+    {
+        if (m_CurrentIndex < StoreManagerAPI.Store.Styles.Count - 1)
+            m_CurrentIndex++;
+        else
+            m_CurrentIndex = 0;
+
+        SetupStyle(StoreManagerAPI.Store.Styles[m_CurrentIndex]);
+    }
+
+    private void OnSwipeRight()
+    {
+        if (m_CurrentIndex > 0)
+            m_CurrentIndex--;
+        else
+            m_CurrentIndex = StoreManagerAPI.Store.Styles.Count - 1;
+
+        SetupStyle(StoreManagerAPI.Store.Styles[m_CurrentIndex]);
+    }
+
+    private void SetupStyle(StoreItem item)
+    {
+        CosmeticData cosmetic = DownloadedAssets.GetCosmetic(item.id);
+        Debug.Log("setup style " + item.id);
+    }
+}
