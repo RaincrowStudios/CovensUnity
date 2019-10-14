1f8d2555a7c8dc243c8bf7f4736dc993674d0dc8
lucas henriques <lucas@oktagongames.com>
2019-10-14 11:08:46 -0300

    remove hardcoded asset versions

--- Covens/Assets/Scripts/Game/StartUp/v2/DownloadManager.cs
+++ Covens/Assets/Scripts/Game/StartUp/v2/DownloadManager.cs
@@ -10,11 +10,11 @@ public class DownloadManager : MonoBehaviour
 {
     public struct AssetResponse
     {
-        public string game => "127";
-        public string store => "117";
+        public string game;// => "127";
+        public string store;// => "117";
         public string localization;
 
-        public List<string> assets => new List<string>() { "spirits-3", "spells-2", "apparel-6", "icon-6", "icon-8", "icon-9", "icon-10", "spells-3" };
+        public List<string> assets;// => new List<string>() { "spirits-3", "spells-2", "apparel-6", "icon-6", "icon-8", "icon-9", "icon-10", "spells-3" };
         public string version;
         public int android;
         public int apple;
