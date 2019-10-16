336cd6c30e7fff4d5b43778615cff3c34aa99224
lucas henriques <lucas@oktagongames.com>
2019-10-16 10:15:55 -0300

    add panel to show channeling fx on main screen's energy bar

--- Covens/Assets/Scripts/Game/UIHelpers/UIMain.cs
+++ Covens/Assets/Scripts/Game/UIHelpers/UIMain.cs
@@ -6,9 +6,60 @@ using TMPro;
 using Raincrow.Chat;
 using Raincrow.Chat.UI;
 using Raincrow.FTF;
+using Raincrow.GameEventResponses;
 
 public class UIMain : MonoBehaviour
 {
+    [System.Serializable]
+    public struct TextPanel
+    {
+        [SerializeField] public TextMeshProUGUI m_Title;
+        [SerializeField] public TextMeshProUGUI m_Content;
+        [SerializeField] public Button m_Button;
+
+        [SerializeField] private RectTransform m_Panel;
+        [SerializeField] private CanvasGroup m_CanvasGroup;
+
+        private int m_TweenId;
+
+        public bool IsOpen { get; private set; }
+        
+        public void Show(bool show)
+        {
+            IsOpen = show;
+
+            LeanTween.cancel(m_TweenId);
+
+            CanvasGroup cg = m_CanvasGroup;
+            RectTransform rt = m_Panel;
+            RectTransform contentRT = m_Content.GetComponent<RectTransform>();
+            float topOffset = m_Title.fontSize * 1f;
+
+            float startAlpha = cg.alpha;
+            float endAlpha = show ? 1 : 0;
+
+            Vector2 startPosition = rt.anchoredPosition;
+            Vector2 endPosition = show ? new Vector2(0, -70) : new Vector2(0, rt.sizeDelta.y);
+            
+            if (show)
+                rt.gameObject.SetActive(true);
+
+            m_TweenId = LeanTween.value(0, 1, 0.5f)
+                .setEaseOutCubic()
+                .setOnUpdate((float t) =>
+                {
+                    cg.alpha = Mathf.Lerp(startAlpha, endAlpha, t);
+                    rt.anchoredPosition = Vector2.Lerp(startPosition, endPosition, t);
+                    rt.sizeDelta = contentRT.sizeDelta + new Vector2(0, topOffset);
+                })
+                .setOnComplete(() =>
+                {
+                    rt.gameObject.SetActive(show);
+                })
+                .uniqueId;
+        }
+    }
+
     public static UIMain Instance { get; private set; }
     
     [Header("Buttons")]
@@ -26,6 +77,9 @@ public class UIMain : MonoBehaviour
     [SerializeField] private Button m_LeaderboardButton;
     [SerializeField] private Button m_ChatButton;
     [SerializeField] private Button m_NearbyPopsButton;
+
+    [Header("Buttons2")]
+    [SerializeField] private Button m_EnergyButton;
     
     [Header("Screens")]
     [SerializeField] private GameObject m_BookOfShadows;
@@ -33,9 +87,9 @@ public class UIMain : MonoBehaviour
 
     [Header("FX")]
     [SerializeField] private GameObject m_ChannelingGlow;
+    [SerializeField] private TextPanel m_EnergyTextPanel;
 
-    private bool m_IsChanneled;
-
+    private bool m_ChanneledFX;
     
     private void Awake()
     {
@@ -55,6 +109,15 @@ public class UIMain : MonoBehaviour
         m_LeaderboardButton.onClick.AddListener(OnClickLeaderboards);
         m_ChatButton.onClick.AddListener(OnClickChat);
         m_NearbyPopsButton.onClick.AddListener(OnClickNearbyPops);
+
+        m_EnergyButton.onClick.AddListener(OnClickEnergy);
+        m_EnergyTextPanel.m_Button.onClick.AddListener(() => m_EnergyTextPanel.Show(false));
+
+        SpellCastHandler.OnApplyEffect += OnApplyStatusEffect;
+        SpellCastHandler.OnExpireEffect += OnExpireStatusEffect;
+
+        m_EnergyTextPanel.Show(false);
+        m_ChannelingGlow.SetActive(false);
     }
 
     private void OnClickLeaderboards()
@@ -158,12 +221,50 @@ public class UIMain : MonoBehaviour
         UINearbyLocations.Open();
     }
 
+    ///////////////////////////////////////////
+    
+    private void OnClickEnergy()
+    {
+        m_EnergyTextPanel.Show(!m_EnergyTextPanel.IsOpen);
+    }
+
     private void OnApplyStatusEffect(string target, string caster, StatusEffect effect)
     {
         if (target != PlayerDataManager.playerData.instance)
             return;
 
-        //if (PlayerManager.witchMarker.witchToken.)
-        //if (PlayerManager.witchMarker.has)
+        if (effect.HasStatus(SpellData.CHANNELED_STATUS))
+        {
+            if (m_ChanneledFX == false)
+            {
+                //enable channeled fx
+                m_ChannelingGlow.gameObject.SetActive(true);
+
+                //setup channeling panel
+                m_EnergyTextPanel.m_Title.text = LocalizeLookUp.GetSpellName("spell_channeling");
+                m_EnergyTextPanel.m_Content.text =
+                       "+" + effect.modifiers.power + " " + LocalizeLookUp.GetText("generic_power") +
+                       "\n+" + effect.modifiers.resilience + " " + LocalizeLookUp.GetText("generic_resilience") +
+                       "\n+" + effect.modifiers.toCrit + "% " + LocalizeLookUp.GetText("generic_critical_cast_rate");
+
+            }
+        }
+    }
+
+    private void OnExpireStatusEffect(string target, StatusEffect effect)
+    {
+        if (target != PlayerDataManager.playerData.instance)
+            return;
+
+        if (effect.HasStatus(SpellData.CHANNELED_STATUS))
+        {
+            //disable channeled fx
+            m_ChannelingGlow.gameObject.SetActive(false);
+
+            //setup channeling panel
+            m_EnergyTextPanel.m_Title.text = LocalizeLookUp.GetText("lt_none");
+            m_EnergyTextPanel.m_Content.text = " ";
+            m_EnergyTextPanel.Show(false);
+        }
     }
 }
