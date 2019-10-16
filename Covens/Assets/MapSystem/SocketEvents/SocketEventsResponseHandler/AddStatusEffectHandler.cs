7c0a4f1cafa1ea4b56218716f646c0721d5cfc06
lucas henriques <lucas@oktagongames.com>
2019-10-16 17:34:12 -0300

    add "add.effect" handler

---
+++ Covens/Assets/MapSystem/SocketEvents/SocketEventsResponseHandler/AddStatusEffectHandler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Newtonsoft.Json;
+using Raincrow.Maps;
+using UnityEngine;
+using TMPro;
+
+namespace Raincrow.GameEventResponses
+{
+    public class AddStatusEffectHandler : IGameEventHandler
+    {
+        private struct AddEventData
+        {
+            [JsonProperty("_id")]
+            public string id;
+            public StatusEffect appliedEffect;
+        }
+
+        public string EventName => "add.effect";
+
+        public void HandleResponse(string eventData)
+        {
+            AddEventData data = JsonConvert.DeserializeObject<AddEventData>(eventData);
+            MarkerSpawner.ApplyStatusEffect(data.id, null, data.appliedEffect);
+        }
+    }
+}
