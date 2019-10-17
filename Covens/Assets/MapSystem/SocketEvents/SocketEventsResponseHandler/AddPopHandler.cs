ca3501e42b1ee0dff781114d4f073994f2188153
lucas henriques <lucas@oktagongames.com>
2019-10-17 08:21:22 -0300

    add "add.token.pop" event handler

---
+++ Covens/Assets/MapSystem/SocketEvents/SocketEventsResponseHandler/AddPopHandler.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using Raincrow.Maps;
+using UnityEngine;
+
+namespace Raincrow.GameEventResponses
+{
+    public class AddPopHandler : AddTokenHandler
+    {
+        public override string EventName => "add.token.pop";
+
+        public override void HandleResponse(string eventData)
+        {
+            PopToken pop = JsonConvert.DeserializeObject<PopToken>(eventData);
+            HandleEvent(pop);
+        }
+    }
+}
