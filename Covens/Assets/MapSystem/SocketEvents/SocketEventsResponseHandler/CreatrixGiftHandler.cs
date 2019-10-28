e8ab02aed7c1bc2fec367a02c50722578c02820d
lucas henriques <lucas@oktagongames.com>
2019-10-28 14:27:00 -0300

    add "creatrix.gift" event handler

---
+++ Covens/Assets/MapSystem/SocketEvents/SocketEventsResponseHandler/CreatrixGiftHandler.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using Newtonsoft.Json;
+using Raincrow.Maps;
+using TMPro;
+
+namespace Raincrow.GameEventResponses
+{
+    public class CreatrixGiftHandler : IGameEventHandler
+    {
+        private struct GiftData
+        {
+            public string id;
+            public int gold;
+            public int silver;
+            public int energy;
+            public double timestamp;
+        }
+
+        public string EventName => "creatrix.gift";
+
+        public void HandleResponse(string eventData)
+        {
+            GiftData data = JsonConvert.DeserializeObject<GiftData>(eventData);
+
+            if (data.id == PlayerDataManager.playerData.instance)
+            {
+                IMarker marker = PlayerManager.marker;
+                OnMapEnergyChange.ForceEvent(PlayerManager.marker, PlayerDataManager.playerData.energy + data.energy, data.timestamp);
+                PlayerDataManager.playerData.AddCurrency(data.silver, data.gold);
+            }
+            else
+            {
+                IMarker marker = MarkerSpawner.GetMarker(data.id);
+                if (marker != null && marker is CharacterMarker)
+                {
+                    CharacterMarker character = marker as CharacterMarker;
+                    OnMapEnergyChange.ForceEvent(character, (character.Token as CharacterToken).energy +  data.energy, data.timestamp);
+                }
+            }
+        }
+    }
+}
