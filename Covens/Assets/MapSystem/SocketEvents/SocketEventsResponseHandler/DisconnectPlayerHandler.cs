dcff08d7f3bca25b57e2b877a26e89025a2d51d5
lucas henriques <lucas@oktagongames.com>
2019-10-31 17:46:50 -0300

    add disconnect player event handler

---
+++ Covens/Assets/MapSystem/SocketEvents/SocketEventsResponseHandler/DisconnectPlayerHandler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using Raincrow.Maps;
+
+namespace Raincrow.GameEventResponses
+{
+    public class DisconnectPlayerHandler : IGameEventHandler
+    {
+        public string EventName => "disconnect.player";
+
+        public void HandleResponse(string eventData)
+        {
+            UIGlobalPopup.ShowPopUp(() =>
+            {
+                Application.Quit();
+
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+#endif
+
+                //LoadingOverlay.Show();
+                //SceneManager.LoadSceneAsync(SceneManager.Scene.START, UnityEngine.SceneManagement.LoadSceneMode.Single, null, () => 
+                //{
+                //    LoadingOverlay.Hide();
+                //    SocketClient.Instance?.DisconnectFromSocket();
+                //    PlayerDataManager.playerData = null;
+                //    LoginAPIManager.loginToken = null;
+                //    LoginAPIManager.wssToken = null;
+                //});
+            },
+            LocalizeLookUp.GetText("server_maintenance"));
+        }
+    }
+}
