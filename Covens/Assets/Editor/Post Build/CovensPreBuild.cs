8a2592805180d66f843edcd5de952a2c857dc6d0
lucas henriques <lucas@oktagongames.com>
2019-11-01 18:37:44 -0300

    update base buildNumber in CovensPreBuild script

--- Covens/Assets/Editor/Post Build/CovensPreBuild.cs
+++ Covens/Assets/Editor/Post Build/CovensPreBuild.cs
@@ -10,7 +10,7 @@ public class CovensPreBuild : MonoBehaviour
         {
             int iBuildNumber = 0;
             int.TryParse(manifest.GetValue<string>("buildNumber"), out iBuildNumber);
-            iBuildNumber += 1000;
+            iBuildNumber += 1050;
             Debug.Log("[CovensPreBuild] buildNumber: " + iBuildNumber);
 #if UNITY_ANDROID
             PlayerSettings.Android.bundleVersionCode = iBuildNumber;
