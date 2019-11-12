aca20106e4c92469ba09ca6d4cc442675cbf6b56
lucas henriques <lucas@oktagongames.com>
2019-11-12 09:04:05 -0300

    change shout's InputField to TMP_Inputfield, small visual adjusts to shout ui

--- Covens/Assets/Scripts/Game/ShoutBox.cs
+++ Covens/Assets/Scripts/Game/ShoutBox.cs
@@ -4,47 +4,58 @@ using System.Collections;
 using Newtonsoft.Json;
 using Raincrow.GameEventResponses;
 using Raincrow.FTF;
+using TMPro;
 
 public class ShoutBox : MonoBehaviour
 {
-
     public Button shoutButton;
-    public GameObject sendButton;
-    public InputField inputField;
+    public Button sendButton;
+    public TMP_InputField inputField;
 
-    bool show = false;
-    public void OnShout()
+    private bool m_IsShowing = false;
+
+    private void Awake()
     {
+        shoutButton.onClick.AddListener(OnClickShout);
+        sendButton.onClick.AddListener(OnClickSend);
 
+        inputField.gameObject.SetActive(false);
+        sendButton.gameObject.SetActive(false);
+        shoutButton.gameObject.SetActive(true);
+    }
+
+    public void OnClickShout()
+    {
         if (FirstTapManager.IsFirstTime("shout"))
         {
-            FirstTapManager.Show("shout", OnShout);
+            FirstTapManager.Show("shout", OnClickShout);
             return;
         }
 
-        if (!show)
+        if (!m_IsShowing)
         {
-            //			shoutButton.gameObject.SetActive (false);
             inputField.gameObject.SetActive(true);
-            sendButton.SetActive(true);
-            //			shoutButton.interactable = false;
+            sendButton.gameObject.SetActive(true);
         }
         else
         {
             inputField.gameObject.SetActive(false);
-            sendButton.SetActive(false);
+            sendButton.gameObject.SetActive(false);
         }
 
-        show = !show;
+        m_IsShowing = !m_IsShowing;
+        inputField.text = "";
     }
 
-    public void OnSend()
+    public void OnClickSend()
     {
         inputField.gameObject.SetActive(false);
-        sendButton.SetActive(false);
+        sendButton.gameObject.SetActive(false);
+        m_IsShowing = false;
 
         string message = inputField.text;
         string data = $"{{\"message\":\"{message}\"}}";
+        inputField.text = "";
 
         APIManager.Instance.Post("character/shout", data, (response, result) =>
         {
@@ -57,15 +68,15 @@ public class ShoutBox : MonoBehaviour
                 Debug.LogError("shout error\n" + response);
             }
         });
+
+        shoutButton.interactable = false;
         StartCoroutine(ReEnableSendButton());
     }
 
     IEnumerator ReEnableSendButton()
     {
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(2);
         shoutButton.interactable = true;
     }
-
-
 }
 
