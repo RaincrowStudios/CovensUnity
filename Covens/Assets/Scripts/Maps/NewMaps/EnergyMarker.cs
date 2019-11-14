7b80c2376b004a6ff280ea69b33ff1dd28bd94d1
lucas henriques <lucas@oktagongames.com>
2019-11-14 08:57:10 -0300

    replace tween animation with unity animation on energy marker, adjust to some texture import settings

--- Covens/Assets/Scripts/Maps/NewMaps/EnergyMarker.cs
+++ Covens/Assets/Scripts/Maps/NewMaps/EnergyMarker.cs
@@ -5,21 +5,8 @@ using TMPro;
 
 public class EnergyMarker : CollectableMarker
 {
-    [Header("Energy Marker")]
-    [SerializeField] private TextMeshPro m_EnergyAmount;
-    //public EnergyToken EnergyTokenData { get; private set; }
-
     public override void Setup(Token data)
     {
         base.Setup(data);
-        //EnergyTokenData = data as EnergyToken;
-
-#if UNITY_EDITOR
-        //  Debug.Log("<color=red>todo</color>: add new socket message for add.token.energy instead of using add.token.item");
-#endif
-        if (data is EnergyToken)
-            m_EnergyAmount.text = (data as EnergyToken).amount.ToString();
-        else if (data is CollectableToken)
-            m_EnergyAmount.text = (data as CollectableToken).amount.ToString();
     }
 }
