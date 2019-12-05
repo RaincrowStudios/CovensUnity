3a2514dfca019dd856f2d5cbb58d351376262c6f
lucas henriques <lucas@oktagongames.com>
2019-12-05 17:30:48 -0300

    implement add add.token.loot socket event

---
+++ Covens/Assets/MapSystem/SocketEvents/SocketEventsResponseHandler/AddLootHandler.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using Raincrow.Maps;
+using UnityEngine;
+
+namespace Raincrow.GameEventResponses
+{
+    public class AddLootHandler : AddTokenHandler
+    {
+        public override string EventName => "add.token.loot";
+
+        public override void HandleResponse(string eventData)
+        {
+            LootToken loot = JsonConvert.DeserializeObject<LootToken>(eventData);
+            HandleEvent(loot);
+        }
+    }
+}
