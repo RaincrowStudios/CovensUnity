06b6284a8bf870cc3fceb9caf3450d2d2dd24ee1
lucas henriques <lucas@oktagongames.com>
2019-12-16 10:58:49 -0300

    dont add the rewards localy on season.reward

--- Covens/Assets/MapSystem/SocketEvents/SocketEventsResponseHandler/SeasonRewardHandler.cs
+++ Covens/Assets/MapSystem/SocketEvents/SocketEventsResponseHandler/SeasonRewardHandler.cs
@@ -28,21 +28,21 @@ namespace Raincrow.GameEventResponses
         {
             EventData data = JsonConvert.DeserializeObject<EventData>(json);
 
-            if (data.items != null)
-            {
-                foreach (var item in data.items)
-                    PlayerDataManager.playerData.AddIngredient(item.id, item.amount);
-            }
+            //if (data.items != null)
+            //{
+            //    foreach (var item in data.items)
+            //        PlayerDataManager.playerData.AddIngredient(item.id, item.amount);
+            //}
 
-            if (data.cosmetics != null)
-            {
-                foreach (var item in data.cosmetics)
-                    PlayerDataManager.playerData.inventory.AddCosmetic(item);
-            }
+            //if (data.cosmetics != null)
+            //{
+            //    foreach (var item in data.cosmetics)
+            //        PlayerDataManager.playerData.inventory.AddCosmetic(item);
+            //}
 
-            PlayerDataManager.playerData.AddCurrency(data.silver, data.gold);
-            PlayerDataManager.playerData.basePower += data.power;
-            PlayerDataManager.playerData.baseResilience += data.resilience;
+            //PlayerDataManager.playerData.AddCurrency(data.silver, data.gold);
+            //PlayerDataManager.playerData.basePower += data.power;
+            //PlayerDataManager.playerData.baseResilience += data.resilience;
 
             UISeasonRewardsPopup.Instantiate().Show(data);
         }
