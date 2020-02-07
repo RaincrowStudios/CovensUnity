5eb3b7a2a5739cfd02e92f53c3c2f16fd8a64df4
Emanuel Tavares <emanuel.oktagon@gmail.com>
2020-02-07 15:02:30 -0300

    Organize script folders and delete TurnModel

--- Covens/Assets/Battle Arena System/Scripts/Models/Data/ITurnModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Raincrow.BattleArena.Model
-{
-    public interface ITurnModel
-    {
-        ICharacterModel[] Character { get; }
-        int ActionsPerTurnCount { get; }
-        float TurnLimit { get; }
-    }
-}
