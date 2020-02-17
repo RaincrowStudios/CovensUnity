9c8e5c17c262e7e1d25b34a59ebedadc7e7c48dc
Emanuel Tavares <emanuel.oktagon@gmail.com>
2020-02-17 17:56:08 -0300

    Merge BattleSpiritView multiple implementations

--- Covens/Assets/Battle Arena System/Scripts/Battle Arena/Views/AbstractCharacterView.cs
+++ Covens/Assets/Battle Arena System/Scripts/Battle Arena/Views/AbstractCharacterView.cs
@@ -4,11 +4,8 @@ namespace Raincrow.BattleArena.View
 {
     public abstract class AbstractCharacterView : MonoBehaviour
     {
-        [SerializeField] protected SpriteRenderer m_EnergyRing;
-
-        [SerializeField] protected MeshRenderer m_AvatarRender;
-        
         public abstract void FaceCamera(Quaternion cameraRotation, Vector3 cameraForward);
+
         public abstract void ChangeCharacterTexture(Texture texture);
 
         public virtual void UpdateEnergy(float time = 1f)
--- Covens/Assets/Battle Arena System/Scripts/Battle Arena/Views/BattleSpiritView.cs
+++ Covens/Assets/Battle Arena System/Scripts/Battle Arena/Views/BattleSpiritView.cs
@@ -1,12 +1,67 @@
-﻿using UnityEngine;
+﻿using Raincrow.BattleArena.Model;
+using UnityEngine;
 
 namespace Raincrow.BattleArena.View
 {
     public class BattleSpiritView : AbstractCharacterView
-    {        
+    {
+        [SerializeField] protected SpriteRenderer m_EnergyRing;
+        [SerializeField] protected MeshRenderer m_AvatarRenderer;
+
+        private ISpiritModel _SpiritModel;
+        private float m_EnergyFill = 0;
+        private int m_EnergyRingTweenId;
+
+        public void Init(ICharacterModel characterModel)
+        {
+            _SpiritModel = ((ISpiritModel)characterModel);
+            SetupCharacter();
+            UpdateEnergy(0);
+        }
+
+        private void SetupCharacter()
+        {
+            m_AvatarRenderer.material.color = new Color(1, 1, 1, 0);
+            //m_EnergyRing.color = spiritToken.IsBossSummon ? new Color(1, 0, 0.2f) : Color.white;
+
+            DownloadedAssets.GetSprite(_SpiritModel.Texture, (sprite) =>
+            {
+                if (m_AvatarRenderer != null && sprite != null)
+                {
+                    float spriteHeight = sprite.rect.height / sprite.pixelsPerUnit;
+                    float spriteWidth = sprite.rect.width / sprite.pixelsPerUnit;
+                    m_AvatarRenderer.transform.localScale = new Vector3(spriteWidth * 5.5f, spriteHeight * 5.5f, 0);
+                    //m_AvatarRender.transform.localPosition = new Vector3(0, spriteHeight * 0.4f * m_AvatarRender.transform.localScale.x, 0);
+                    m_AvatarRenderer.material.SetTexture("_MainTex", sprite.texture);
+                    LeanTween.color(
+                        m_AvatarRenderer.gameObject,
+                        Color.white, 1f
+                    ).setEaseOutCubic();
+                }
+            });
+        }
+
+        public override void UpdateEnergy(float time = 1f)
+        {
+            LeanTween.cancel(m_EnergyRingTweenId);
+
+            m_EnergyFill = _SpiritModel.Energy;
+            m_EnergyFill /= _SpiritModel.BaseEnergy;
+
+            if (Mathf.Approximately(time, 0f))
+            {
+                m_EnergyRing.color = new Color(1, 1, 1, m_EnergyFill);
+            }
+            else
+            {
+                m_EnergyRingTweenId = LeanTween.alpha(m_EnergyRing.gameObject, m_EnergyFill, time).uniqueId;
+            }
+        }        
+
         public override void FaceCamera(Quaternion cameraRotation, Vector3 cameraForward)
         {
-            
+            Vector3 worldPosition = transform.position + cameraRotation * Vector3.forward;
+            m_AvatarRenderer.transform.LookAt(worldPosition, cameraForward);
         }
 
         public override void ChangeCharacterTexture(Texture texture)
