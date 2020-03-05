a5b4e78f476e340900478f18017924b7afd1f439
Emanuel Tavares <emanuel.oktagon@gmail.com>
2020-03-05 16:35:23 -0300

    Add Smooth Follow Camera

---
+++ Covens/Assets/Battle Arena System/Utils/SmoothCameraFollow.cs
@@ -0,0 +1,28 @@
+﻿// Smooth towards the target
+
+using UnityEngine;
+
+namespace Raincrow.Utils
+{
+    public class SmoothCameraFollow : MonoBehaviour
+    {
+        [SerializeField] private Transform _target;
+        [SerializeField] private float _smoothTime = 0.3F;
+        [SerializeField] private float _cameraHeight = 5f;
+        [SerializeField] private float _cameraDistance = 10f;
+
+        private Vector3 velocity = Vector3.zero;
+
+        protected void Update()
+        {
+            // Define a target position above and behind the target transform
+            Vector3 targetPosition = _target.TransformPoint(new Vector3(0, _cameraHeight, -_cameraDistance));
+
+            // Smoothly move the camera towards that target position
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, _smoothTime);
+            
+            // Look at target
+            transform.LookAt(_target);
+        }
+    }
+}
