8b6a8e17356bf402beaf4334756f6e2d345000ec
Renan Laureano <renanlaureano@gmail.com>
2020-03-20 12:10:10 -0300

    Add join request and popup error on join and challenge

--- Covens/Assets/Battle Arena System/Scripts/Player/ChallengeRequests.cs
+++ Covens/Assets/Battle Arena System/Scripts/Player/ChallengeRequests.cs
@@ -5,7 +5,7 @@ using Newtonsoft.Json;
 
 namespace BattleArena
 {
-   public static class ChallengeRequests
+    public static class ChallengeRequests
     {
         public static void Challenge(string id, System.Action success = null, System.Action error = null)
         {
@@ -19,6 +19,25 @@ namespace BattleArena
                     }
                     else
                     {
+                        UIGlobalPopup.ShowPopUp(() => { }, "It was not possible to challenge");
+                        error?.Invoke();
+                    }
+                });
+        }
+
+        public static void Join(string id, System.Action success = null, System.Action error = null)
+        {
+            APIManager.Instance.Post(
+                "battle/join/" + id, "{}",
+                (response, result) =>
+                {
+                    if (result == 200)
+                    {
+                        success?.Invoke();
+                    }
+                    else
+                    {
+                        UIGlobalPopup.ShowPopUp(() => { }, "Could not connect to the server!");
                         error?.Invoke();
                     }
                 });
